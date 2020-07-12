cb7a04161ceed78016458fa8b9f6400da04c9def
gdittrick <gjdittrick@pyramidsystems.com>
2020-07-12 10:03:48 -0400

    added branch name modifier

--- groovy/jobs/deploy.groovy
+++ groovy/jobs/deploy.groovy
@@ -9,12 +9,12 @@ node {
     stage("Deploy Database") {
         if( fileExists('database.yml')) {
             sh "kubectl apply -n zevrant-home-services-$ENVIRONMENT -f ./database.yml"
-            sh "kubectl rollout status deployments $REPOSITORY-db-deployment"
+            sh "kubectl rollout status deployments $REPOSITORY-db-deployment -n zevrant-home-services-$ENVIRONMENT"
         }
     }
 
     stage("Deploy") {
         sh "VERSION=$VERSION envsubst < deployment.yml | ENVIRONMENT=$ENVIRONMENT envsubst | kubectl apply -n zevrant-home-services-$ENVIRONMENT -f -"
-        sh "kubectl rollout status deployments $REPOSITORY-deployment"
+        sh "kubectl rollout status deployments $REPOSITORY-deployment -n zevrant-home-services-$ENVIRONMENT"
     }
 }
