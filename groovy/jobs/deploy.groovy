f9115603a73d1c754dbfa334c654c778209d1271
gdittrick <gjdittrick@pyramidsystems.com>
2020-07-12 15:15:00 -0400

    added credentials manager stuff

--- groovy/jobs/deploy.groovy
+++ groovy/jobs/deploy.groovy
@@ -12,7 +12,9 @@ node {
             sh "kubectl rollout status deployments $REPOSITORY-db-deployment -n zevrant-home-services-$ENVIRONMENT"
             print "Deploying liquibase updates"
             def serviceName = REPOSITORY.tokenize("-")[1]
-            def POSTGRESS_PASSWORD = credentials("/$ENVIRONMENT/rds/$serviceName/password")
+            def POSTGRESS_PASSWORD = sh returnStdout: true, script: "aws secretsmanager get-secret-value --secret-id /$ENVIRONMENT/rds/$serviceName/password | | jq .SecretString")
+            POSTGRESS_PASSWORD = POSTGRESS_PASSWORD.replaceAll(Pattern.compile("\""), "");
+            print POSTGRESS_PASSWORD;
             sh "POSTGRES_PASSWORD=$POSTGRESS_PASSWORD ./gradlew liquibase update"
         }
     }
