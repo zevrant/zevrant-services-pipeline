598a5175a513e892cdbd28e4c7de093f28b86852
gdittrick <gjdittrick@pyramidsystems.com>
2020-07-12 09:55:01 -0400

    added branch name modifier

--- groovy/jobs/deploy.groovy
+++ groovy/jobs/deploy.groovy
@@ -2,12 +2,15 @@
 node {
 
     stage("SCM Checkout") {
-        git credentialsId: 'jenkins-git', branch: ENVIRONMENT,
+        git credentialsId: 'jenkins-git',
                 url: "git@github.com:zevrant/${REPOSITORY}.git"
     }
 
     stage("Deploy Database") {
-        sh "set +e; kubectl apply -n zevrant-home-services-$ENVIRONMENT -f ./database.yml; set -e; kubectl rollout status deployment.v1.apps/$REPOSITORY-db-deployment"
+        if( fileExists('database.yml')) {
+            sh "kubectl apply -n zevrant-home-services-$ENVIRONMENT -f ./database.yml"
+            sh "kubectl rollout status deployment.v1.apps/$REPOSITORY-db-deployment"
+        }
     }
 
     stage("Deploy") {
