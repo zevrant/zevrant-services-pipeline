3f7372a940a2fa71452aeafbfc3ac79e27d968c0
gdittrick <gjdittrick@pyramidsystems.com>
2020-07-13 15:53:49 -0400

    added credentials manager stuff

--- groovy/jobs/deploy.groovy
+++ groovy/jobs/deploy.groovy
@@ -10,13 +10,13 @@ node {
     stage("Deploy Database") {
         if( fileExists('database.yml')) {
             print ENVIRONMENT
-            sh "ENVIRONMENT=$ENVIRONMENT envsubst < database.yml | kubectl apply -n zevrant-home-services-$ENVIRONMENT -f ./database.yml"
+            sh "export ENVIRONMENT=$ENVIRONMENT; envsubst < database.yml | kubectl apply -n zevrant-home-services-$ENVIRONMENT -f ./database.yml"
             sh "kubectl rollout status deployments $REPOSITORY-db-deployment -n zevrant-home-services-$ENVIRONMENT"
         }
     }
 
     stage("Deploy") {
-        sh "VERSION=$VERSION envsubst < deployment.yml | ENVIRONMENT=$ENVIRONMENT envsubst | kubectl apply -n zevrant-home-services-$ENVIRONMENT -f -"
+        sh "export VERSION=$VERSION; export ENVIRONMENT=$ENVIRONMENT; envsubst < deployment.yml | kubectl apply -n zevrant-home-services-$ENVIRONMENT -f -"
         sh "kubectl rollout status deployments $REPOSITORY-deployment -n zevrant-home-services-$ENVIRONMENT"
     }
 }
