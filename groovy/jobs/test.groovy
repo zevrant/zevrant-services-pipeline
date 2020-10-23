bf5c04b8d6ccb61f0ec771c4530397dbbc1fe92a
zevrant <gerethd@gmail.com>
2020-10-23 11:05:05 -0400

    added html publish

--- groovy/jobs/test.groovy
+++ groovy/jobs/test.groovy
@@ -9,4 +9,15 @@ node {
     stage("Test") {
         sh "./gradlew clean test aggregate -Denvironment=develop -Dspring.profiles.active=develop --no-daemon"
     }
+
+    stage("publish") {
+        publishHTML(target: [
+                reportName : 'Serenity',
+                reportDir:   'target/site/serenity',
+                reportFiles: 'index.html',
+                keepAll:     true,
+                alwaysLinkToLastBuild: true,
+                allowMissing: false
+        ])
+    }
 }
