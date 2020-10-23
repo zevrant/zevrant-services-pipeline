ced609b3cda547070b1782633c6a1c87f5200700
zevrant <gerethd@gmail.com>
2020-10-23 09:59:44 -0400

    added automation test job

---
+++ groovy/jobs/test.groovy
@@ -0,0 +1,12 @@
+
+node {
+
+    stage("SCM Checkout") {
+        git credentialsId: 'jenkins-git', branch: master,
+                url: "git@github.com:zevrant/zevrant-automation-tests.git"
+    }
+
+    stage("Test") {
+        sh "./gradlew clean test aggregate -Denvironment=develop"
+    }
+}
