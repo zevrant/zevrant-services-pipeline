42ea946d84c0b3e5d027982f669fb87d0d085729
zevrant <gerethd@gmail.com>
2020-10-23 10:48:29 -0400

    fixed branch ref

--- groovy/jobs/test.groovy
+++ groovy/jobs/test.groovy
@@ -2,7 +2,7 @@
 node {
 
     stage("SCM Checkout") {
-        git credentialsId: 'jenkins-git', branch: master,
+        git credentialsId: 'jenkins-git', branch: "master",
                 url: "git@github.com:zevrant/zevrant-automation-tests.git"
     }
 
