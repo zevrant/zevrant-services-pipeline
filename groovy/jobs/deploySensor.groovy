d94c453b6fabfb9f8acac188e87eb33d47b77fbf
Zevrant <workmailtome@gmail.com>
2021-02-02 20:04:49 -0500

    FT_ZHS-25 finished deploy sensor script

--- groovy/jobs/deploySensor.groovy
+++ groovy/jobs/deploySensor.groovy
@@ -4,17 +4,33 @@ node {
     withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-git', keyFileVariable: 'key', passphraseVariable: 'password', usernameVariable: 'username')]) {
         // some block
 
-        def sensorLocations = ["192.168.1.20"];
-        def baseSSHCommand = "ssh -i $key zevrant-sensor-service@";
-        for(def sensorLocation : sensorLocations) {
+        String[] sensorLocations = ["192.168.1.20"];
+        String baseSSHCommand = "ssh -i $key zevrant-sensor-service@" as String;
+        for (String sensorLocation : sensorLocations) {
             stage("Download Artifact") {
                 sh "${baseSSHCommand}${sensorLocation} 'aws s3 cp s3://zevrant-artifact-store/com/zevrant/services/${REPOSITORY}/${version}/${REPOSITORY}-${version}.jar .'"
                 sh "${baseSSHCommand}${sensorLocation} 'ln -sf ${REPOSITORY}-${version}.jar ${REPOSITORY}.jar"
             }
 
             stage("Start Service") {
+                sh "${baseSSHCommand}${sensorLocation} 'sudo systemctl restart zevrant-sensor-service"
+                for (def int i = 0; i < 10; i++) {
+                    try {
+                        def response = sh returnStdout: true, script: "curl https://${sensorLocation}:9006/zevrant-sensor-service/actuator/health"
 
+                        def jsonString = sh returnStdout: true, script: "aws ssm get-parameter --name ${REPOSITORY}-VERSION";
+                        JsonSlurper slurper = new JsonSlurper();
+                        Map parsedJson = slurper.parseText(jsonString);
+                        def status = parsedJson.get("status");
+                        if(status.equals("UP")) {
+                            break;
+                        }
+                    } catch (Exception ex) {
+                        Thread.sleep(5000)
+                    }
+                }
             }
         }
+
     }
 }
