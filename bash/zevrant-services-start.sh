3577b0e48f0f4fc02242e63d8e89514ff26e9290
Zevrant <workmailtome@gmail.com>
2021-02-25 12:31:55 -0500

    FT_ZHS-30 added service name for kubernetes name resolution

--- bash/zevrant-services-start.sh
+++ bash/zevrant-services-start.sh
@@ -17,6 +17,7 @@ setAwsCredentials "$aws_credentials"
 credentials=`aws sts assume-role --role-arn arn:aws:iam::725235728275:role/OauthServiceRole --role-session-name startup | jq .Credentials`
 setAwsCredentials "$credentials"
 sed -i "s/\${POD_IP}/$POD_IP/g" ~/openssl.conf
+sed -i "s/\${SERVICE_NAME}/$1/g" ~/openssl.conf
 cat ~/openssl.conf
 openssl req -newkey rsa:4096 -nodes -keyout ~/private.pem -days 365 -out ~/public.csr -config ~/openssl.conf
 username=`aws secretsmanager get-secret-value --region us-east-1 --secret-id certificateUsername | jq .SecretString`
@@ -28,4 +29,4 @@ certificateRequest=`printf "%q" "$certificateRequest"`
 certificateRequest=`echo $certificateRequest | cut -c 3-$((${#certificateRequest}-1))`
 certificateRequest="{\"certificateRequest\":\"$certificateRequest\",\"ip\":\"$POD_IP\"}"
 curl --insecure https://192.168.1.17:9009/zevrant-certificate-service/certs --data "$certificateRequest" --user $username:$password -H "Content-Type: application/json" -X POST > ~/public.crt
-openssl pkcs12 -export -inkey ~/private.pem -in ~/public.crt -passout "pass:$1" -out ~/zevrant-services.p12
+openssl pkcs12 -export -inkey ~/private.pem -in ~/public.crt -passout "pass:$2" -out ~/zevrant-services.p12
