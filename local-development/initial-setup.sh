44b89ae3d145fac4fd368d7a4013ed514b9d310f
Zevrant <workmailtome@gmail.com>
2021-05-16 11:41:59 -0400

    changes topic name

--- local-development/initial-setup.sh
+++ local-development/initial-setup.sh
@@ -2,7 +2,7 @@
 
 docker-compose -f docker/kafka.yml up -d
 
-kafka-topics.sh --create --topic zevrant-sensor-stream --zookeeper localhost:2181 --partitions 1 --replication-factor 1
+kafka-topics.sh --create --topic zevrant-video-stream --zookeeper localhost:2181 --partitions 1 --replication-factor 1
 kafka-topics.sh --list --zookeeper localhost:2181
 
 
