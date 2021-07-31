f64f851c530f471d53d9b1404c6326721240d4f7
Zevrant <workmailtome@gmail.com>
2021-07-31 13:42:16 -0400

    fixed default pipeline parameter construcor calls

--- jenkins/seed.groovy
+++ jenkins/seed.groovy
@@ -18,7 +18,7 @@ PipelineCollection.pipelines.each { pipeline ->
                 pipeline.parameters.each { parameter ->
                     switch (parameter.type) {
                         case String.class:
-                            stringParameter(parameter.name, parameter.defaultValue, parameter.description)
+                            stringParam(parameter.name, parameter.defaultValue, parameter.description)
                             break;
                         case Boolean.class:
 
