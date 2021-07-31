6ce4dadef2c281af789d421f36bbbfde0c85136a
Zevrant <workmailtome@gmail.com>
2021-07-31 13:21:26 -0400

    removed use of streams

--- jenkins/seedJob.groovy
+++ jenkins/seedJob.groovy
@@ -12,7 +12,7 @@ node("master") {
 
     String script = "";
 
-    stage("Assemble Seec File") {
+    stage("Assemble Seed File") {
         script += processLibraryCode(readFile("jenkins/src/main/groovy/com/zevrant/services/Pipeline.groovy"))
         println script
         script += "\n" + readFile("jenkins/seed.groocy")
@@ -32,7 +32,5 @@ node("master") {
 }
 
 static String processLibraryCode(String libraryCode) {
-    return Arrays.asList(libraryCode.split('\n')).stream().filter { line ->
-        return line.contains("package com.zevrant.services")
-    }.collect(Collectors.toList()).join("\n");
+    return libraryCode.substring(libraryCode.indexOf("import"));
 }
