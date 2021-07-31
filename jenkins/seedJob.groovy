b51574e275365d6e7055362b253335d84687156d
Zevrant <workmailtome@gmail.com>
2021-07-31 11:54:41 -0400

    removed node specifier

--- jenkins/seedJob.groovy
+++ jenkins/seedJob.groovy
@@ -2,62 +2,59 @@ import com.zevrant.services.PipelineCollection
 
 @Library("CommonUtils")
 
-node("master") {
+PipelineCollection.pipelines.each { pipeline ->
 
-    PipelineCollection.pipelines.each { pipeline ->
+    pipelineJob(pipeline.name) {
+        description pipeline.description
+        String jobDisplayName = ""
+        pipeline.name.split("-").each {piece ->
+            displayName += piece.capitalize() + " "
+        }
 
-        pipelineJob(pipeline.name) {
-            description pipeline.description
-            String jobDisplayName = ""
-            pipeline.name.split("-").each {piece ->
-                displayName += piece.capitalize() + " "
-            }
+        displayName jobDisplayName.trim()
 
-            displayName jobDisplayName.trim()
+        logRotator {
+            numToKeep 20
+        }
 
-            logRotator {
-                numToKeep 20
-            }
+        if(pipeline.parameters != null && pipeline.parameters.length > 0) {
+            parameters() {
+                pipeline.parameters.each { parameter ->
+                    switch (parameter.type) {
+                        case Class<String>: {
+                            stringPrameter(parameter.name, parameter.defaultValue, parameter.description)
+                            break;
+                        } case Class<Boolean>: {
 
-            if(pipeline.parameters != null && pipeline.parameters.length > 0) {
-                parameters() {
-                    pipeline.parameters.each { parameter ->
-                        switch (parameter.type) {
-                            case Class<String>: {
-                                stringPrameter(parameter.name, parameter.defaultValue, parameter.description)
-                                break;
-                            } case Class<Boolean>: {
-
-                                break;
-                            } case Class<List<? extends Object>> : {
-                                break;''
-                            }
+                            break;
+                        } case Class<List<? extends Object>> : {
+                            break;''
                         }
                     }
                 }
             }
+        }
 
-            definition {
-                cpsScmFlowDefinition {
-                    scm {
-                        gitScm {
-                            userRemoteConfigs {
-                                name('origin')
-                                url(pipeline.gitRepo)
-                                credentialsId(pipeline.credentialId)
-                                refspec('+refs/heads/master:refs/remotes/origin/master')
-                            }
+        definition {
+            cpsScmFlowDefinition {
+                scm {
+                    gitScm {
+                        userRemoteConfigs {
+                            name('origin')
+                            url(pipeline.gitRepo)
+                            credentialsId(pipeline.credentialId)
+                            refspec('+refs/heads/master:refs/remotes/origin/master')
                         }
-
-                        branches {
-                            branchSpec {
-                                name('master')
-                            }
-                        }
-
-                        scriptPath(pipeline.jenkisfileLocation)
-                        lightweight(true)
                     }
+
+                    branches {
+                        branchSpec {
+                            name('master')
+                        }
+                    }
+
+                    scriptPath(pipeline.jenkisfileLocation)
+                    lightweight(true)
                 }
             }
         }
