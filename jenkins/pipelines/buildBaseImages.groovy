2706f52df24036a498170ad6388f8246764e61b6
Zevrant <workmailtome@gmail.com>
2021-08-07 12:58:03 -0400

    added more parallelization

--- jenkins/pipelines/buildBaseImages.groovy
+++ jenkins/pipelines/buildBaseImages.groovy
@@ -25,20 +25,25 @@ node("master") {
     stage("Get Potential Repos") {
         def response = httpRequest authentication: 'jenkins-git-access-token', url: "https://api.github.com/orgs/zevrant/repos?type=all"
         List jsonResponse = readJSON text: response.content
+        def parralelSteps = [:]
         jsonResponse.each { repo ->
                     if ((repo['name'] as String).contains('zevrant')
                             && repo['name'] as String != 'zevrant-services-pipeline'
                             && !(repo['archived'] as Boolean)) {
                         branchesToBuild.each({branch ->
-                            def dockerfileResponse = httpRequest(authentication: 'jenkins-git-access-token',
-                                    contentType: "TEXT_PLAIN",
-                                    validResponseCodes: "200:404",
-                                    url: "https://raw.githubusercontent.com/zevrant/${repo['name'] as String}/master/Dockerfile")
-                            if(dockerfileResponse.status < 400) {
-                                for (image in imagesToBuild) {
-                                    if(dockerfileResponse.content.contains(image)) {
-                                        affectedRepos.get(branch).add(repo['name'] as String);
-                                        break;
+                            parralelSteps["${repo['name']}:$branch"] = {
+                                stage("Get repo branch list") {
+                                    def dockerfileResponse = httpRequest(authentication: 'jenkins-git-access-token',
+                                            contentType: "TEXT_PLAIN",
+                                            validResponseCodes: "200:404",
+                                            url: "https://raw.githubusercontent.com/zevrant/${repo['name'] as String}/master/Dockerfile")
+                                    if (dockerfileResponse.status < 400) {
+                                        for (image in imagesToBuild) {
+                                            if (dockerfileResponse.content.contains(image)) {
+                                                affectedRepos.get(branch).add(repo['name'] as String);
+                                                break;
+                                            }
+                                        }
                                     }
                                 }
                             }
@@ -47,6 +52,7 @@ node("master") {
 
                     }
                 }
+        parallel parralelSteps
     }
 
     stage("Rebuild Downstream Repos") {
