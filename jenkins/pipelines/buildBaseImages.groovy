04780cae37100524530b7bd06d5d15f1f99abddd
Zevrant <workmailtome@gmail.com>
2021-08-07 12:31:50 -0400

    added logic to look for affected repos

--- jenkins/pipelines/buildBaseImages.groovy
+++ jenkins/pipelines/buildBaseImages.groovy
@@ -17,7 +17,8 @@ node("master") {
 //        }
     }
 
-    stage("Update Downstream Repos") {
+    List<String> affectedRepos = new ArrayList<>();
+    stage("Get Potential Repos") {
         def response = httpRequest authentication: 'jenkis-git-access-token', url: "https://api.github.com/orgs/zevrant/repos?type=all"
         List jsonResponse = readJSON text: response.content
         jsonResponse.stream()
@@ -25,8 +26,23 @@ node("master") {
                     if ((repo['name'] as String).contains('zevrant')
                             && repo['name'] as String != 'zevrant-services-pipeline'
                             && !(repo['archived'] as Boolean)) {
-                        println repo['name']
+                        def dockerfileResponse = httpRequest authentication: 'jenkins-git-access-token',
+                                contentType: "TEXT_PLAIN"
+                                url: "https://raw.githubusercontent.com/zevrant/${repo['name'] as String}/master/Dockerfile"
+                        if(dockerfileResponse.status < 400) {
+                            for (image in imagesToBuild) {
+                                if(dockerfileResponse.content.contains(image)) {
+                                    affectedRepos.add(repo['name'] as String);
+                                    break;
+                                }
+                            }
+                        }
+
                     }
                 }
+    }
+
+    stage("Update Downstream Repos") {
+        println "MicroServices to update ${affectedRepos}"
     }
 }
