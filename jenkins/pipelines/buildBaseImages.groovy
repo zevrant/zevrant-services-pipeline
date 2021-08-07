859a9a12bac22eb23bb9027f0396a147c867a62f
Zevrant <workmailtome@gmail.com>
2021-08-07 10:54:02 -0400

    added git checkout and stages

--- jenkins/pipelines/buildBaseImages.groovy
+++ jenkins/pipelines/buildBaseImages.groovy
@@ -2,10 +2,17 @@
 def imagesToBuild = ["zevrant-ubuntu-base"];
 
 node("master") {
-    dir("docker/dockerfile") {
-        imagesToBuild.each { image ->
-            sh "docker build . -t zevrant/${image}:latest -f ${image}.dockerfile --no-cache"
-            sh "docker push zevrant/${image}:latest"
+
+    stage("Git Checkout") {
+        git(url: 'git@github.com:zevrant/zevrant-services-pipeline.git', credentialsId: 'jenkins-git', branch: 'master')
+    }
+
+    stage("Build & Push Dockerfiles") {
+        dir("docker/dockerfile") {
+            imagesToBuild.each { image ->
+                sh "docker build . -t zevrant/${image}:latest -f ${image}.dockerfile --no-cache"
+                sh "docker push zevrant/${image}:latest"
+            }
         }
     }
 }
