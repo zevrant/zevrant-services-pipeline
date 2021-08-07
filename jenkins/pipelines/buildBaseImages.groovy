8ca5767f2bff673d98114dd23e92d2f897f44f42
Zevrant <workmailtome@gmail.com>
2021-08-07 12:55:12 -0400

    added build/deploy stage in parallel

--- jenkins/pipelines/buildBaseImages.groovy
+++ jenkins/pipelines/buildBaseImages.groovy
@@ -1,6 +1,7 @@
 import java.util.List
 
-def imagesToBuild = ["zevrant-ubuntu-base"];
+def imagesToBuild = ["zevrant-ubuntu-base"]
+def branchesToBuild = ["develop"]
 
 node("master") {
 
@@ -17,7 +18,10 @@ node("master") {
 //        }
     }
 
-    List<String> affectedRepos = [];
+    Map<String, List<String>> affectedRepos = new HashMap<>();
+    branchesToBuild.each({ branch ->
+        affectedRepos.put(branch, []);
+    })
     stage("Get Potential Repos") {
         def response = httpRequest authentication: 'jenkins-git-access-token', url: "https://api.github.com/orgs/zevrant/repos?type=all"
         List jsonResponse = readJSON text: response.content
@@ -25,24 +29,40 @@ node("master") {
                     if ((repo['name'] as String).contains('zevrant')
                             && repo['name'] as String != 'zevrant-services-pipeline'
                             && !(repo['archived'] as Boolean)) {
-                        def dockerfileResponse = httpRequest(authentication: 'jenkins-git-access-token',
-                                contentType: "TEXT_PLAIN",
-                                validResponseCodes: "200:404",
-                                url: "https://raw.githubusercontent.com/zevrant/${repo['name'] as String}/master/Dockerfile")
-                        if(dockerfileResponse.status < 400) {
-                            for (image in imagesToBuild) {
-                                if(dockerfileResponse.content.contains(image)) {
-                                    affectedRepos.add(repo['name'] as String);
-                                    break;
+                        branchesToBuild.each({branch ->
+                            def dockerfileResponse = httpRequest(authentication: 'jenkins-git-access-token',
+                                    contentType: "TEXT_PLAIN",
+                                    validResponseCodes: "200:404",
+                                    url: "https://raw.githubusercontent.com/zevrant/${repo['name'] as String}/master/Dockerfile")
+                            if(dockerfileResponse.status < 400) {
+                                for (image in imagesToBuild) {
+                                    if(dockerfileResponse.content.contains(image)) {
+                                        affectedRepos.get(branch).add(repo['name'] as String);
+                                        break;
+                                    }
                                 }
                             }
-                        }
+                        })
+
 
                     }
                 }
     }
 
-    stage("Update Downstream Repos") {
-        println "MicroServices to update ${affectedRepos}"
+    stage("Rebuild Downstream Repos") {
+        def buildJobs = [:];
+        branchesToBuild.each { branch ->
+            println ("Build $branch branch for these repositories")
+
+            buildJobs["Build $branch for ${affectedRepos.get(branch)}"] = {
+                stage("Build $branch for ${affectedRepos.get(branch)}") {
+                    build job: 'Build', parameters: [
+                            [$class: 'StringParameterValue', name: 'REPOSITORY', value: affectedRepos.get(branch)],
+                            [$class: 'StringParameterValue', name: 'BASE_BRANCH', value: "refs/heads/$branch"]
+                    ]
+                }
+            }
+        }
+        parallel buildJobs
     }
 }
