8f8c8b1a4aea84189a4837782bb1a04aaeeeca52
Zevrant <workmailtome@gmail.com>
2021-08-07 12:42:19 -0400

    fixed access token id

--- jenkins/pipelines/buildBaseImages.groovy
+++ jenkins/pipelines/buildBaseImages.groovy
@@ -19,7 +19,7 @@ node("master") {
 
     List<String> affectedRepos = [];
     stage("Get Potential Repos") {
-        def response = httpRequest authentication: 'jenkis-git-access-token', url: "https://api.github.com/orgs/zevrant/repos?type=all"
+        def response = httpRequest authentication: 'jenkins-git-access-token', url: "https://api.github.com/orgs/zevrant/repos?type=all"
         List jsonResponse = readJSON text: response.content
         jsonResponse.each { repo ->
                     if ((repo['name'] as String).contains('zevrant')
@@ -28,14 +28,14 @@ node("master") {
                         def dockerfileResponse = httpRequest(authentication: 'jenkins-git-access-token',
                                 contentType: "TEXT_PLAIN",
                                 url: "https://raw.githubusercontent.com/zevrant/${repo['name'] as String}/master/Dockerfile")
-//                        if(dockerfileResponse.status < 400) {
-//                            for (image in imagesToBuild) {
-//                                if(dockerfileResponse.content.contains(image)) {
-////                                    affectedRepos.add(repo['name'] as String);
-//                                    break;
-//                                }
-//                            }
-//                        }
+                        if(dockerfileResponse.status < 400) {
+                            for (image in imagesToBuild) {
+                                if(dockerfileResponse.content.contains(image)) {
+                                    affectedRepos.add(repo['name'] as String);
+                                    break;
+                                }
+                            }
+                        }
 
                     }
                 }
