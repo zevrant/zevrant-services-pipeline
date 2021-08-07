8343095ec7dfa47bf912df6f5825e8437d42f351
Zevrant <workmailtome@gmail.com>
2021-08-07 10:51:45 -0400

    fixed borked docker build command

--- jenkins/pipelines/buildBaseImages.groovy
+++ jenkins/pipelines/buildBaseImages.groovy
@@ -4,7 +4,7 @@ def imagesToBuild = ["zevrant-ubuntu-base"];
 node("master") {
     dir("docker/dockerfile") {
         imagesToBuild.each { image ->
-            sh "docker build -t zevrant/${image}:latest -f ${image}.dockerfile --no-cache"
+            sh "docker build . -t zevrant/${image}:latest -f ${image}.dockerfile --no-cache"
             sh "docker push zevrant/${image}:latest"
         }
     }
