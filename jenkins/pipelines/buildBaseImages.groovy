7f8ab59ca50b13bb01519f1dc029bc5489f62427
Zevrant <workmailtome@gmail.com>
2021-08-07 10:56:13 -0400

    added poull command

--- jenkins/pipelines/buildBaseImages.groovy
+++ jenkins/pipelines/buildBaseImages.groovy
@@ -10,7 +10,7 @@ node("master") {
     stage("Build & Push Dockerfiles") {
         dir("docker/dockerfile") {
             imagesToBuild.each { image ->
-                sh "docker build . -t zevrant/${image}:latest -f ${image}.dockerfile --no-cache"
+                sh "docker build . -t zevrant/${image}:latest -f ${image}.dockerfile --no-cache --pull"
                 sh "docker push zevrant/${image}:latest"
             }
         }
