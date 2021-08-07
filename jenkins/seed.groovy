4aa9d605e129f8c4454e47538c388886439210a9
Zevrant <workmailtome@gmail.com>
2021-08-07 17:38:54 -0400

    debugging

--- jenkins/seed.groovy
+++ jenkins/seed.groovy
@@ -2,77 +2,77 @@ import com.zevrant.services.PipelineCollection
 
 PipelineCollection.pipelines.each { pipeline ->
 
-//    pipelineJob(pipeline.name) {
-//        description pipeline.description
-//        String jobDisplayName = ""
-//        pipeline.name.split("-").each { piece ->
-//            jobDisplayName += piece.capitalize() + " "
-//        }
-//
-//        displayName(jobDisplayName.trim())
-//        disabled pipeline.disabled
-//        logRotator {
-//            numToKeep pipeline.buildsToKeep
-//        }
-//
-//        if (pipeline.parameters != null && pipeline.parameters.size() > 0) {
-//            parameters() {
-//                pipeline.parameters.each { parameter ->
-//                    switch (parameter.type) {
-//                        case String.class:
-//                            stringParam(parameter.name, parameter.defaultValue, parameter.description)
-//                            break;
-//                        case Boolean.class:
-//                            booleanParam(parameter.name, parameter.defaultValue, parameter.description)
-//                            break;
-//                        case List.class:
-//                            choiceParam(parameter.name, parameter.defaultValue, parameter.description)
-//                            break;
-//                        default:
-//                            throw RuntimeException("Parameter not supported")
-//                    }
-//                }
-//            }
-//        }
-//        if(pipeline.triggers.size() > 0) {
-//            triggers {
-//                pipeline.triggers.each { trigger ->
-//                    switch (trigger.type) {
-//                        case CRON:
-//                            cron(trigger.value);
-//                            break;
-//                        default:
-//                            throw new RuntimeException("Pipeline Trigger Type Not Implemented ${trigger.Type} for pipeline ${pipeline.name}")
-//                    }
-//                }
-//            }
-//        }
-//
-//        definition {
-//            cpsScm {
-//                lightweight(true)
-//                scm {
-//                    git {
-//                        remote {
-//                            credentials(pipeline.credentialId)
-//                            name('origin')
-//                            url(pipeline.gitRepo)
-//                        }
-//
-//                        branch('master')
-//
-//                        browser {
-//                            gitWeb("https://github.com:zevrant/zevrant-services-pipeline")
-//                        }
-//                        extensions {
-//                            cloneOptions {
-//                                shallow(true)
-//                            }
-//                        }
-//                    }
-//                }
-//                scriptPath(pipeline.jenkinsfileLocation)
-//            }
-//        }
-//    }
+    pipelineJob(pipeline.name) {
+        description pipeline.description
+        String jobDisplayName = ""
+        pipeline.name.split("-").each { piece ->
+            jobDisplayName += piece.capitalize() + " "
+        }
+
+        displayName(jobDisplayName.trim())
+        disabled pipeline.disabled
+        logRotator {
+            numToKeep pipeline.buildsToKeep
+        }
+
+        if (pipeline.parameters != null && pipeline.parameters.size() > 0) {
+            parameters() {
+                pipeline.parameters.each { parameter ->
+                    switch (parameter.type) {
+                        case String.class:
+                            stringParam(parameter.name, parameter.defaultValue, parameter.description)
+                            break;
+                        case Boolean.class:
+                            booleanParam(parameter.name, parameter.defaultValue, parameter.description)
+                            break;
+                        case List.class:
+                            choiceParam(parameter.name, parameter.defaultValue, parameter.description)
+                            break;
+                        default:
+                            throw RuntimeException("Parameter not supported")
+                    }
+                }
+            }
+        }
+        if(pipeline.triggers.size() > 0) {
+            triggers {
+                pipeline.triggers.each { trigger ->
+                    switch (trigger.type) {
+                        case CRON:
+                            cron(trigger.value);
+                            break;
+                        default:
+                            throw new RuntimeException("Pipeline Trigger Type Not Implemented ${trigger.Type} for pipeline ${pipeline.name}")
+                    }
+                }
+            }
+        }
+
+        definition {
+            cpsScm {
+                lightweight(true)
+                scm {
+                    git {
+                        remote {
+                            credentials(pipeline.credentialId)
+                            name('origin')
+                            url(pipeline.gitRepo)
+                        }
+
+                        branch('master')
+
+                        browser {
+                            gitWeb("https://github.com:zevrant/zevrant-services-pipeline")
+                        }
+                        extensions {
+                            cloneOptions {
+                                shallow(true)
+                            }
+                        }
+                    }
+                }
+                scriptPath(pipeline.jenkinsfileLocation)
+            }
+        }
+    }
 }
