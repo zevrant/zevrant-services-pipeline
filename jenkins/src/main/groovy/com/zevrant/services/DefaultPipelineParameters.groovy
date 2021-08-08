48c45cafbcb21ccb71af990526b7c1252a0d91b2
Zevrant <workmailtome@gmail.com>
2021-08-08 13:29:42 -0400

    fixed default parameter names to match variable name

--- jenkins/src/main/groovy/com/zevrant/services/DefaultPipelineParameters.groovy
+++ jenkins/src/main/groovy/com/zevrant/services/DefaultPipelineParameters.groovy
@@ -1,8 +1,8 @@
 package com.zevrant.services
 
 enum DefaultPipelineParameters {
-    REPOSITORY_PARAMETER(new PipelineParameter<String>(String.class, "BRANCH_NAME", "Branch to be built", "")),
-    BRANCH_PARAMETER(new PipelineParameter<String>(String.class, "REPOSITORY", "Git repository to be built", "master"));
+    REPOSITORY_PARAMETER(new PipelineParameter<String>(String.class, "REPOSITORY", "Branch to be built", "")),
+    BRANCH_PARAMETER(new PipelineParameter<String>(String.class, "BRANCH_NAME", "Git repository to be built", "master"));
 
     PipelineParameter parameter;
 
