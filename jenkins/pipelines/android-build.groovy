4af4bcaf391f9562182a08ecf620c72f93275f76
Zevrant <workmailtome@gmail.com>
2021-08-14 19:03:46 -0400

    set name and label on upload

--- jenkins/pipelines/android-build.groovy
+++ jenkins/pipelines/android-build.groovy
@@ -56,7 +56,7 @@ node("master") {
         ).content)
         println "uploading to " + response['assets_url']
         withCredentials([usernamePassword(credentialsId: 'jenkins-git-access-token', passwordVariable: 'password', usernameVariable: 'username')]) {
-            sh "curl -H 'Authorization: token: $password' -H 'Content-Type: application/vnd.android.package-archive' --data-binary @app/build/outputs/apk/release/app-release-unsigned.apk ${response['assets_url']}"
+            sh "curl -H 'Authorization: token: $password' -H 'Content-Type: application/vnd.android.package-archive' --data-binary @app/build/outputs/apk/release/app-release-unsigned.apk ${response['assets_url']}/?name=zevrant-services.apk&label=zevrant-services.apk\n"
         }
 
     }
