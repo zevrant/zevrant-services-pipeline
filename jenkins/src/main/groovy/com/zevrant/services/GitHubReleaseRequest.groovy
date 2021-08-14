111a2a68ca01f94ac02dafeacd1bc021c95cb30c
Zevrant <workmailtome@gmail.com>
2021-08-14 17:49:04 -0400

    make fields public

--- jenkins/src/main/groovy/com/zevrant/services/GitHubReleaseRequest.groovy
+++ jenkins/src/main/groovy/com/zevrant/services/GitHubReleaseRequest.groovy
@@ -1,31 +1,15 @@
 package com.zevrant.services;
 
 class GitHubReleaseRequest {
-    private final String tag_name;
-    private final boolean prerelease;
-    private final boolean draft;
-    private final String name;
+    final String tag_name;
+    final boolean prerelease;
+    final boolean draft;
+    final String name;
 
     GitHubReleaseRequest(String tag_name, boolean prerelease, boolean draft, String name) {
         this.tag_name = tag_name;
         this.prerelease = prerelease;
         this.draft = draft;
         this.name = name;
-    }
-
-    String getTagName() {
-        return tag_name;
-    }
-
-    boolean isPrerelease() {
-        return prerelease;
-    }
-
-    boolean isDraft() {
-        return draft;
-    }
-
-    String getName() {
-        return name;
     }
 }
