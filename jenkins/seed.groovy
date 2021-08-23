70b3908c836a94e769daaf001862c4529e316b04
Zevrant <workmailtome@gmail.com>
2021-08-23 10:01:23 -0400

    fixed display name variable

--- jenkins/seed.groovy
+++ jenkins/seed.groovy
@@ -51,7 +51,7 @@ String jobName = "zevrant-android-app"
 String displayNameString = ""
 folder("Android") {}
 String folderName = "Android/"
-jobName.split("-").each { repositoryName -> displayName += repositoryName.capitalize() + " " }
+jobName.split("-").each { repositoryName -> displayNameString += repositoryName.capitalize() + " " }
 jobName = jobName.trim()
 folderName += jobName + "/"
 folder(folderName.substring(0, folderName.length() -1)) {}
