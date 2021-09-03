b5938292a9a1c63772532c3d2d08e9fd1b112835
Zevrant <workmailtome@gmail.com>
2021-09-03 15:51:42 -0400

    added script and steps blocks

--- jenkins/pipelines/androidBuild.groovy
+++ jenkins/pipelines/androidBuild.groovy
@@ -12,56 +12,84 @@ pipeline {
 
     stages {
         stage("Get Version") {
-            def json = readJSON text: (sh(returnStdout: true, script: "aws ssm get-parameter --name ${repository}-VERSION"))
-            version = json['Parameter']['Value']
+            steps {
+                script {
+                    def json = readJSON text: (sh(returnStdout: true, script: "aws ssm get-parameter --name ${repository}-VERSION"))
+                    version = json['Parameter']['Value']
+                }
+            }
         }
 
 
         stage("SCM Checkout") {
-            git credentialsId: 'jenkins-git', branch: BRANCH_NAME,
-                    url: "git@github.com:zevrant/${REPOSITORY}.git"
+            steps {
+                script {
+                    git credentialsId: 'jenkins-git', branch: BRANCH_NAME,
+                            url: "git@github.com:zevrant/${REPOSITORY}.git"
+                }
+            }
         }
 
         stage("Test") {
-            "bash gradlew clean build --no-daemon"
+            steps {
+                script {
+                    "bash gradlew clean build --no-daemon"
+                }
+            }
         }
 
         stage("Release Version Update") {
             when { expression { variant == 'release' } }
-                def splitVersion = version.tokenize(".");
-                def majorVersion = splitVersion[0]
-                majorVersion = majorVersion.toInteger() + 1
-                def newVersion = "${majorVersion}.0.0";
-            sh "aws ssm put-parameter --name ${REPOSITORY}-VERSION --value $version --type String --overwrite"
+            steps {
+                script {
+                    def splitVersion = version.tokenize(".");
+                    def majorVersion = splitVersion[0]
+                    majorVersion = majorVersion.toInteger() + 1
+                    def newVersion = "${majorVersion}.0.0";
+                    sh "aws ssm put-parameter --name ${REPOSITORY}-VERSION --value $version --type String --overwrite"
+                }
+            }
         }
 
         stage("Development Version Update") {
             when { expression { variant != 'release' } }
-            def splitVersion = version.tokenize(".");
-            def minorVersion = splitVersion[2]
-            minorVersion = minorVersion.toInteger() + 1
-            version = "${splitVersion[0]}.${splitVersion[1]}.${minorVersion}"
-            sh "aws ssm put-parameter --name ${REPOSITORY}-VERSION --value $version --type String --overwrite"
+            steps {
+                script {
+                    def splitVersion = version.tokenize(".");
+                    def minorVersion = splitVersion[2]
+                    minorVersion = minorVersion.toInteger() + 1
+                    version = "${splitVersion[0]}.${splitVersion[1]}.${minorVersion}"
+                    sh "aws ssm put-parameter --name ${REPOSITORY}-VERSION --value $version --type String --overwrite"
+                }
+            }
         }
 
         stage("Build Artifact") {
-            def json = readJSON text: (sh(returnStdout: true, script: "aws secretsmanager get-secret-value --secret-id /android/signing/keystore"))
-            String keystore = json['SecretString']; writeFile file: './zevrant-services.txt', text: keystore
-            sh "base64 -d ./zevrant-services.txt > ./zevrant-services.p12"
-            json = readJSON text: (sh(returnStdout: true, script: "aws secretsmanager get-secret-value --secret-id /android/signing/password"))
-            String password = json['SecretString']
-            sh " SIGNING_KEYSTORE=\'${env.WORKSPACE}/zevrant-services.p12\' " + 'KEYSTORE_PASSWORD=\'' + password + "\' bash gradlew clean assemble${variant.capitalize()} --no-daemon"
-            //for some reason gradle isn't signing like it's suppost to so we do it manually
-            sh "keytool -v -importkeystore -srckeystore zevrant-services.p12 -srcstoretype PKCS12 -destkeystore zevrant-services.jks -deststoretype JKS -srcstorepass \'$password\' -deststorepass \'$password\' -noprompt"
+            steps {
+                script {
+                    def json = readJSON text: (sh(returnStdout: true, script: "aws secretsmanager get-secret-value --secret-id /android/signing/keystore"))
+                    String keystore = json['SecretString']; writeFile file: './zevrant-services.txt', text: keystore
+                    sh "base64 -d ./zevrant-services.txt > ./zevrant-services.p12"
+                    json = readJSON text: (sh(returnStdout: true, script: "aws secretsmanager get-secret-value --secret-id /android/signing/password"))
+                    String password = json['SecretString']
+                    sh " SIGNING_KEYSTORE=\'${env.WORKSPACE}/zevrant-services.p12\' " + 'KEYSTORE_PASSWORD=\'' + password + "\' bash gradlew clean assemble${variant.capitalize()} --no-daemon"
+                    //for some reason gradle isn't signing like it's suppost to so we do it manually
+                    sh "keytool -v -importkeystore -srckeystore zevrant-services.p12 -srcstoretype PKCS12 -destkeystore zevrant-services.jks -deststoretype JKS -srcstorepass \'$password\' -deststorepass \'$password\' -noprompt"
 
-            sh "zipalign -p -f -v 4 app/build/outputs/apk/$variant/app-${variant}.apk zevrant-services-unsigned.apk"
-            sh "apksigner sign --ks zevrant-services.jks --in ./zevrant-services-unsigned.apk --out ./zevrant-services.apk --ks-pass \'pass:$password\'"
-            sh "apksigner verify -v zevrant-services.apk"
+                    sh "zipalign -p -f -v 4 app/build/outputs/apk/$variant/app-${variant}.apk zevrant-services-unsigned.apk"
+                    sh "apksigner sign --ks zevrant-services.jks --in ./zevrant-services-unsigned.apk --out ./zevrant-services.apk --ks-pass \'pass:$password\'"
+                    sh "apksigner verify -v zevrant-services.apk"
+                }
+            }
         }
 
         stage("Release") {
             when { expression { BRANCH_NAME == 'develop' || BRANCH_NAME == 'master'}}
-            sh "aws s3 cp ./zevrant-services.apk s3://zevrant-apk-store/$variant/$version/zevrant-services.apk"
+            steps {
+                script {
+                    sh "aws s3 cp ./zevrant-services.apk s3://zevrant-apk-store/$variant/$version/zevrant-services.apk"
+                }
+            }
         }
     }
 }
