362c4bee309c61875a3974ff8eb7aa0f56085bfe
Zevrant <workmailtome@gmail.com>
2021-09-04 14:30:43 -0400

    fixed unterminated string

--- jenkins/pipelines/androidBuild.groovy
+++ jenkins/pipelines/androidBuild.groovy
@@ -54,7 +54,7 @@ pipeline {
     rm snapshot.zip
     rm -r snapshot/\$folder/""".stripIndent()
                     }
-                    String startEmulator = "/opt/android/android-sdk/emulator/emulator -sysdir /opt/android/android-sdk/system-images/android-30/google_apis_playstore/x86_64/ -avd $avdName' -no-window -no-boot-anim -no-snapshot-save -snapshot snapshot/"
+                    String startEmulator = "/opt/android/android-sdk/emulator/emulator -sysdir /opt/android/android-sdk/system-images/android-30/google_apis_playstore/x86_64/ -avd $avdName -no-window -no-boot-anim -no-snapshot-save -snapshot snapshot/"
                     sh "echo no | /opt/android/android-sdk/cmdline-tools/5.0/bin/avdmanager create avd -n $avdName --abi google_apis_playstore/x86_64 --package \'system-images;android-30;google_apis_playstore;x86_64\'"
                     pid = sh returnStdout: true, script: "nohup $startEmulator &"
                     sh 'bash gradlew clean connectedDevelopTest'
