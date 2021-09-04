a4193be0e76e1aad889209430d4016b176ca6bde
Zevrant <workmailtome@gmail.com>
2021-09-04 14:34:05 -0400

    added secrets initializer

--- jenkins/pipelines/androidBuild.groovy
+++ jenkins/pipelines/androidBuild.groovy
@@ -57,16 +57,22 @@ pipeline {
                     String startEmulator = "/opt/android/android-sdk/emulator/emulator -sysdir /opt/android/android-sdk/system-images/android-30/google_apis_playstore/x86_64/ -avd $avdName -no-window -no-boot-anim -no-snapshot-save -snapshot snapshot/"
                     sh "echo no | /opt/android/android-sdk/cmdline-tools/5.0/bin/avdmanager create avd -n $avdName --abi google_apis_playstore/x86_64 --package \'system-images;android-30;google_apis_playstore;x86_64\'"
                     pid = sh returnStdout: true, script: "nohup $startEmulator &"
+                    sh """
+    set +x
+    secretsInitializer=`aws secretsmanager get-secret-value --region us-east-1 --secret-id android-secrets-initializer | jq .SecretString`
+    secretsInitializer=`echo \$secretsInitializer | cut -c 2-\$((\${#secretsInitializer}-1))`
+    echo \$secretsInitializer | base64 --decode > app/src/androidTest/java/com/zevrant/services/zevrantandroidapp/secrets/SecretsInitializer.java
+                    """
                     sh 'bash gradlew clean connectedDevelopTest'
                 }
             }
             post {
                 always {
                     script {
-                        if(fileExists(junitFileName)) {
+                        if (fileExists(junitFileName)) {
                             junit junitFileName
                         }
-                        if(pid != "" && pid != null) {
+                        if (pid != "" && pid != null) {
                             echo "killing emulator with pid $pid"
                             sh "kill -9 $pid"
                             echo "deleting avd with name $avdName"
