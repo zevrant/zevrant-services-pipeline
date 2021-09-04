7ebd568e9a20df9280070b5fb3760b195ae1b545
Zevrant <workmailtome@gmail.com>
2021-09-04 14:17:38 -0400

    changed zip name and removed unecessary file delete

--- jenkins/pipelines/androidBuild.groovy
+++ jenkins/pipelines/androidBuild.groovy
@@ -45,17 +45,16 @@ pipeline {
         stage("Integration Testing") {
             steps {
                 script {
-                    if (!fileExists('pixel4-snapshot')) {
+                    if (!fileExists('snapshot')) {
                         sh """
-    aws s3 cp s3://zevrant-artifact-store/pixel4-snapshot.zip pixel4-snapshot.zip
-    unzip pixel4-snapshot.zip  -d pixel4-snapshot
-    folder=`ls pixel4-snapshot`
-    cp -r pixel4-snapshot/\$folder/* pixel4-snapshot
-    rm pixel4-snapshot.zip
-    rm pixel4-snapshot.zip*
-    rm -r pixel4-snapshot/\$folder/""".stripIndent()
+    aws s3 cp s3://zevrant-artifact-store/snapshot.zip snapshot.zip
+    unzip snapshot.zip  -d snapshot
+    folder=`ls snapshot`
+    cp -r snapshot/\$folder/* snapshot
+    rm snapshot.zip
+    rm -r snapshot/\$folder/""".stripIndent()
                     }
-                    String startEmulator = "emulator -sysdir /opt/android/android-sdk/system-images/android-30/google_apis_playstore/x86_64/ -avd $avdName' -no-window -no-boot-anim -no-snapshot-save -snapshot pixel4-snapshot/"
+                    String startEmulator = "emulator -sysdir /opt/android/android-sdk/system-images/android-30/google_apis_playstore/x86_64/ -avd $avdName' -no-window -no-boot-anim -no-snapshot-save -snapshot snapshot/"
                     sh "avdmanager create avd -n $avdName --abi google_apis_playstore/x86_64 --package 'system-images;android-30;google_apis_playstore;x86_64'"
                     pid = sh returnStdout: true, script: "nohup $startEmulator &"
                     sh 'bash gradlew clean connectedDevelopTest'
