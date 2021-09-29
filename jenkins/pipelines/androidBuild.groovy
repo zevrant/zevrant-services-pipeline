d14d7f2e3a9573b7be3482440a0b9acf38e80ab1
zevrant <workmailtome@gmail.com>
2021-09-29 13:32:54 -0400

    added taskloader

--- jenkins/pipelines/androidBuild.groovy
+++ jenkins/pipelines/androidBuild.groovy
@@ -1,3 +1,4 @@
+import com.zevrant.services.TaskLoader
 import com.zevrant.services.pojo.Version
 import com.zevrant.services.services.VersionTasks
 
@@ -10,7 +11,7 @@ Version version = null
 String variant = (BRANCH_NAME == "master") ? "release" : 'develop'
 String avdName = "jenkins-android-test-$BUILD_ID"
 String junitFileName = "app/build/outputs/androidTest-results/connected/TEST-${avdName}(AVD) - 11-app-.xml"
-VersionTasks versionTasks = new VersionTasks();
+VersionTasks versionTasks = TaskLoader.load(binding, VersionTasks) as AzCli
 pipeline {
     agent {
         label 'master'
