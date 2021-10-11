d603e081bf9b69c7842e13cc3c3c251245ce6368
zevrant <workmailtome@gmail.com>
2021-10-11 13:51:11 -0400

    switched from using sed to writeFile

--- jenkins/pipelines/androidBuild.groovy
+++ jenkins/pipelines/androidBuild.groovy
@@ -195,8 +195,13 @@ cat secret.txt | base64 --decode > app/src/androidTest/java/com/zevrant/services
                     dir("/opt/fdroid") {
                         sh "rm -rf repo/icon*"
                         sh "fdroid update"
-                        sh "sed -i 's/CurrentVersion: \\\'.*\\\'/CurrentVersion: \\\'${version.toThreeStageVersionString()}\\\'/g' metadata/com.zevrant.services.${(REPOSITORY as String).toLowerCase().replace("-", "")}.${(BRANCH_NAME == "develop"? "develop." : "")}yml"
-                        sh "sed -i 's/CurrentVersionCode: \\\'.*\\\'/CurrentVersionCode: \\\'${versionCode.toVersionCodeString()}\\\'/g' metadata/com.zevrant.services.${(REPOSITORY as String).toLowerCase().replace("-", "")}.${(BRANCH_NAME == "develop"? "develop." : "")}yml"
+                        String fileName = "metadata/com.zevrant.services.${(REPOSITORY as String).toLowerCase().replace("-", "")}.${(BRANCH_NAME == "develop" ? "develop." : "")}yml"
+                        String versionFile = readFile(file: fileName)
+                        versionFile = versionFile
+                                .replaceAll("^CurrentVersion .*", "CurrentVersion: '${version.toThreeStageVersionString()}'")
+                                .replaceAll("^CurrentVersionCode: .*", "CurrentVersionCode: '${versionCode.toVersionCodeString()}'")
+                        writeFile(file: fileName, text: versionFile)
+                    }
                 }
             }
         }
