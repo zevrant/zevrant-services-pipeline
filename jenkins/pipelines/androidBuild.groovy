21b6157a19a72d243c945d92efe8864eabaef81a
zevrant <workmailtome@gmail.com>
2021-10-19 22:47:05 -0400

    set default region correctly

--- jenkins/pipelines/androidBuild.groovy
+++ jenkins/pipelines/androidBuild.groovy
@@ -20,7 +20,7 @@ pipeline {
     environment {
         AWS_ACCESS_KEY_ID= credentials('aws-access-key-id')
         AWS_SECRET_ACCESS_KEY= credentials('aws-secret-access-key')
-        AWS_DEFAULT_REGION="us-east"
+        AWS_DEFAULT_REGION="us-east-1"
     }
     agent {
         docker {
