b07ee5cb0e6061043d0308b1b88eb2d6dde8b808
zevrant <workmailtome@gmail.com>
2021-10-23 11:22:06 -0400

    fixed curly braces

--- jenkins/pipelines/kubernetes-deploy.groovy
+++ jenkins/pipelines/kubernetes-deploy.groovy
@@ -11,35 +11,35 @@ pipeline {
                     script {
                         sh 'printenv'
                         currentBuild.displayName = "Deploying $REPOSITORY version $VERSION"
-                        git credentialsId: 'jenkins-git', branch: (ENVIRONMENT = 'prod')? 'master' : ENVIRONMENT,
+                        git credentialsId: 'jenkins-git', branch: (ENVIRONMENT = 'prod') ? 'master' : ENVIRONMENT,
                                 url: "git@github.com:zevrant/${REPOSITORY}.git"
                     }
                 }
             }
-            stage("Deploy Database") {
-                when { expression { fileExists('database.yml') } }
-                steps {
-                    container('kubernetes') {
-                        script {
-                            print ENVIRONMENT
-                            sh "sed -i 's/\$ENVIRONMENT/$ENVIRONMENT/g' ./database.yml"
-                            sh "kubectl apply -n zevrant-home-services-$ENVIRONMENT -f ./database.yml"
-                            sh "kubectl rollout status deployments $REPOSITORY-db-deployment -n zevrant-home-services-$ENVIRONMENT"
-                        }
+        }
+        stage("Deploy Database") {
+            when { expression { fileExists('database.yml') } }
+            steps {
+                container('kubernetes') {
+                    script {
+                        print ENVIRONMENT
+                        sh "sed -i 's/\$ENVIRONMENT/$ENVIRONMENT/g' ./database.yml"
+                        sh "kubectl apply -n zevrant-home-services-$ENVIRONMENT -f ./database.yml"
+                        sh "kubectl rollout status deployments $REPOSITORY-db-deployment -n zevrant-home-services-$ENVIRONMENT"
                     }
                 }
             }
+        }
 
-            stage("Deploy") {
-                when { expression { fileExists('database.yml') } }
-                steps {
-                    container('kubernetes') {
-                        script {
-                            sh "sed -i 's/\$ENVIRONMENT/$ENVIRONMENT/g' ./deployment.yml"
-                            sh "sed -i 's/\$VERSION/$VERSION/g' ./deployment.yml"
-                            sh "kubectl apply -n zevrant-home-services-$ENVIRONMENT -f ./deployment.yml"
-                            sh "kubectl rollout status deployments $REPOSITORY-deployment -n zevrant-home-services-$ENVIRONMENT"
-                        }
+        stage("Deploy") {
+            when { expression { fileExists('database.yml') } }
+            steps {
+                container('kubernetes') {
+                    script {
+                        sh "sed -i 's/\$ENVIRONMENT/$ENVIRONMENT/g' ./deployment.yml"
+                        sh "sed -i 's/\$VERSION/$VERSION/g' ./deployment.yml"
+                        sh "kubectl apply -n zevrant-home-services-$ENVIRONMENT -f ./deployment.yml"
+                        sh "kubectl rollout status deployments $REPOSITORY-deployment -n zevrant-home-services-$ENVIRONMENT"
                     }
                 }
             }
