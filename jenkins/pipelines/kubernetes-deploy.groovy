c7ac726dd9fa470175fae74344e9b611c2ee3d85
zevrant <workmailtome@gmail.com>
2021-10-23 15:44:55 -0400

    switched to env var kubeconfig

--- jenkins/pipelines/kubernetes-deploy.groovy
+++ jenkins/pipelines/kubernetes-deploy.groovy
@@ -19,13 +19,16 @@ pipeline {
         }
         stage("Deploy Database") {
             when { expression { fileExists('database.yml') } }
+            environment {
+                KUBECONFIG = credentials('jenkins-kubernetes')
+            }
             steps {
                 container('kubectl') {
                     script {
                         sh "sed -i 's/\$ENVIRONMENT/$ENVIRONMENT/g' ./database.yml"
                         withCredentials([file(credentialsId: 'jenkins-kubernetes', variable: 'kubeconfig')]) {
-                            sh "kubectl apply --kubeconfig $kubeconfig -n zevrant-home-services-$ENVIRONMENT -f ./database.yml"
-                            sh "kubectl rollout --kubeconfig $kubeconfig status deployments $REPOSITORY-db-deployment -n zevrant-home-services-$ENVIRONMENT"
+                            sh "kubectl apply --kubeconfig '$kubeconfig' -n zevrant-home-services-$ENVIRONMENT -f ./database.yml"
+                            sh "kubectl rollout --kubeconfig '$kubeconfig' status deployments $REPOSITORY-db-deployment -n zevrant-home-services-$ENVIRONMENT"
                         }
                     }
                 }
@@ -34,15 +37,16 @@ pipeline {
 
         stage("Deploy") {
             when { expression { fileExists('database.yml') } }
+            environment {
+                KUBECONFIG = credentials('jenkins-kubernetes')
+            }
             steps {
                 container('kubectl') {
                     script {
                         sh "sed -i 's/\$ENVIRONMENT/$ENVIRONMENT/g' ./deployment.yml"
                         sh "sed -i 's/\$VERSION/$VERSION/g' ./deployment.yml"
-                        withCredentials([file(credentialsId: 'jenkins-kubernetes', variable: 'kubeconfig')]) {
-                            sh "kubectl $kubeconfig apply --kubeconfig $kubeconfig -n zevrant-home-services-$ENVIRONMENT -f ./deployment.yml"
-                            sh "kubectl rollout --kubeconfig $kubeconfig status deployments $REPOSITORY-deployment -n zevrant-home-services-$ENVIRONMENT"
-                        }
+                        sh "kubectl $kubeconfig apply-n zevrant-home-services-$ENVIRONMENT -f ./deployment.yml"
+                        sh "kubectl rolloutstatus deployments $REPOSITORY-deployment -n zevrant-home-services-$ENVIRONMENT"
                     }
                 }
             }
