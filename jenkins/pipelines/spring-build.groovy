58ed366403ccd8f5850a2cf1891e3d622b4b7aa3
zevrant <workmailtome@gmail.com>
2021-10-25 04:02:18 -0400

    added aws creds to build stage

--- jenkins/pipelines/spring-build.groovy
+++ jenkins/pipelines/spring-build.groovy
@@ -101,6 +101,9 @@ pipeline {
 
         stage("Build Artifact") {
             environment {
+                AWS_ACCESS_KEY_ID = credentials('aws-access-key-id')
+                AWS_SECRET_ACCESS_KEY = credentials('aws-secret-access-key')
+                AWS_DEFAULT_REGION = "us-east-1"
                 DOCKER_TOKEN = credentials('jenkins-dockerhub')
             }
             steps {
