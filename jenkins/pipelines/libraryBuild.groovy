de4e89d665a47b1004986ba924db7081f73a0bb9
zevrant <workmailtome@gmail.com>
2021-10-25 02:49:40 -0400

    added env for artifact publish

--- jenkins/pipelines/libraryBuild.groovy
+++ jenkins/pipelines/libraryBuild.groovy
@@ -88,6 +88,11 @@ pipeline {
         }
 
         stage("Build & Publish") {
+            environment {
+                AWS_ACCESS_KEY_ID = credentials('aws-access-key-id')
+                AWS_SECRET_ACCESS_KEY = credentials('aws-secret-access-key')
+                AWS_DEFAULT_REGION = "us-east-1"
+            }
             steps {
                 container('spring-jenkins-slave') {
                     script {
