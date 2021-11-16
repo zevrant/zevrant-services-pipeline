f079e703e716ff3f0d21789e177c2a684ec15f9d
zevrant <workmailtome@gmail.com>
2021-11-16 17:09:03 -0500

    readded build name update to include version number

--- jenkins/pipelines/libraryBuild.groovy
+++ jenkins/pipelines/libraryBuild.groovy
@@ -48,7 +48,7 @@ pipeline {
                 container('spring-jenkins-slave') {
                     script {
                         version = versionTasks.getVersion(REPOSITORY as String) as Version
-                        currentBuild.displayName "Building Version ${version.toThreeStageVersionString()}"
+                        currentBuild.displayName = "Building Version ${version.toThreeStageVersionString()}" as String
                     }
                 }
             }
