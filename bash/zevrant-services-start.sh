bf7cf5197fb4a13e9c867087309764fb72a0ce56
zevrant <workmailtome@gmail.com>
2021-11-25 14:27:59 -0500

    removed dependency on ec2 metadata

--- bash/zevrant-services-start.sh
+++ bash/zevrant-services-start.sh
@@ -1,21 +1,3 @@
-setAwsCredentials () {
-  export AWS_ACCESS_KEY_ID=$(echo "$1" | jq .AccessKeyId)
-  export AWS_SECRET_ACCESS_KEY=$(echo "$1" | jq .SecretAccessKey)
-  if [[ "$(echo "$1" | jq .Token)" == "null" ]];
-  then
-    export AWS_SESSION_TOKEN=$(echo "$1" | jq .SessionToken)
-  else
-    export AWS_SESSION_TOKEN=$(echo "$1" | jq .Token)
-  fi
-  AWS_ACCESS_KEY_ID=$(echo "$AWS_ACCESS_KEY_ID" | cut -c 2-$((${#AWS_ACCESS_KEY_ID}-1)))
-  AWS_SECRET_ACCESS_KEY=$(echo "$AWS_SECRET_ACCESS_KEY" | cut -c 2-$((${#AWS_SECRET_ACCESS_KEY}-1)))
-  AWS_SESSION_TOKEN=$(echo "$AWS_SESSION_TOKEN" | cut -c 2-$((${#AWS_SESSION_TOKEN}-1)))
-}
-
-aws_credentials=$(curl --proxy "$PROXY_CREDENTIALS"@3.210.165.61:3128 http://169.254.169.254/latest/meta-data/identity-credentials/ec2/security-credentials/ec2-instance)
-setAwsCredentials "$aws_credentials"
-credentials=$(aws sts assume-role --role-arn arn:aws:iam::725235728275:role/SecretsOnlyServiceRole --role-session-name startup | jq .Credentials)
-setAwsCredentials "$credentials"
 curl https://raw.githubusercontent.com/zevrant/zevrant-services-pipeline/master/bash/openssl.conf > ~/openssl.conf
 sed -i "s/\${POD_IP}/$POD_IP/g" ~/openssl.conf
 sed -i "s/\${SERVICE_NAME}/$1/g" ~/openssl.conf
