140670a542c4ed89a500af5448271e8812379e22
zevrant <workmailtome@gmail.com>
2021-12-25 13:56:10 -0500

    added not ready status to list of pods to be deleted and added delete pods role to jenkins

--- jenkins/pipelines/podCleanup.groovy
+++ jenkins/pipelines/podCleanup.groovy
@@ -14,7 +14,8 @@ pipeline {
                 container('kubectl') {
                     script {
                         sh "kubectl get pods --no-headers=true -n jenkins | awk '/Error/{print \$1}' | xargs kubectl delete pods || true"
-                        sh "kubectl get pods --no-headers=true -n jenkins | awk '/Complete/{print \$1}' | xargs kubectl delete pods || true"
+                        sh "kubectl get pods --no-headers=true -n jenkins | awk '/Completed/{print \$1}' | xargs kubectl delete pods || true"
+                        sh "kubectl get pods --no-headers=true -n jenkins | awk '/NotReady/{print \$1}' | xargs kubectl delete pods || true"
                     }
                 }
             }
