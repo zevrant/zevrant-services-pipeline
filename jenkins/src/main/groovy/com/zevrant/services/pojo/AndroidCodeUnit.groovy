8396916acd1e0b8223b8be9a54393a68d1d27e41
gdittrick <gdittrick@zevrant-services.com>
2023-03-10 11:04:44 -0500

    debugging

--- jenkins/src/main/groovy/com/zevrant/services/pojo/AndroidCodeUnit.groovy
+++ jenkins/src/main/groovy/com/zevrant/services/pojo/AndroidCodeUnit.groovy
@@ -1,8 +1,11 @@
 package com.zevrant.services.pojo
 
+import com.zevrant.services.enumerations.ApplicationType
+
 class AndroidCodeUnit extends CodeUnit{
 
     AndroidCodeUnit(Map<String, Object> params) {
         super(params)
+        applicationType = ApplicationType.ANDROID
     }
 }
