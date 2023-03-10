5d27b84a6039aade202cc863607779db9cccbf1b
gdittrick <gdittrick@zevrant-services.com>
2023-03-10 08:57:06 -0500

    updated seed.groovy to have explicit config declaration instead of auto discover

--- jenkins/seedJob.groovy
+++ jenkins/seedJob.groovy
@@ -1,14 +1,8 @@
 @Library('CommonUtils') _
 
-import com.zevrant.services.services.GitService
 import com.zevrant.services.pojo.SpringCodeUnitCollection
 import com.zevrant.services.pojo.LibraryCodeUnitCollection
 
-List<String> libraryRepositories = new ArrayList<>();
-List<String> microserviceRepositories = new ArrayList<>();
-List giteaRepoList = null
-List<String> ignoreRepos = ['zevrant-sensor-service', 'zevrant-security-service']
-GitService gitService = ServiceLoader.load(binding, GitService.class) as GitService
 pipeline {
     agent {
         kubernetes {
@@ -20,6 +14,7 @@ pipeline {
         stage("Process Seed File") {
             steps {
                 script {
+                    sh 'ls -l'
                     jobDsl(
                             targets: 'jenkins/seed.groovy',
                             removedJobAction: 'DELETE',
