4d7607076a5cc465adc84bf848de5b3e1d81064a
gdittrick <gdittrick@zevrant-services.com>
2023-05-10 09:02:13 -0400

    commented out broken code

--- jenkins/src/main/groovy/com/zevrant/services/services/GitService.groovy
+++ jenkins/src/main/groovy/com/zevrant/services/services/GitService.groovy
@@ -20,36 +20,36 @@ void checkout(String hostname = 'ssh://git@ssh.gitea.zevrant-services.com:30121'
     )
 }
 
-oid postBuildPrHook(GitHubRepo gitHubRepo) {
-    GitHubRepo repo = new GitHubRepo(repoSsh)
-    BuildManagement buildManagement = TaskLoader.load(binding, BuildManagement)
-
-    String prNumber = buildManagement.getPrNumber()
-    String credentialsId = gitHubRepo.credentialsId
-
-    if (prNumber) {
-        Map<String, Object> prDetails = getPullRequestDetails(
-                credentialsId,
-                repo.organization,
-                repo.name,
-                prNumber
-        )
-        notifyPrReviewer(prDetails)
-        String color = 'danger'
-        String message = 'Build Failed.'
-
-        if (prDetails.draft) {
-            color = 'warning'
-            message = 'This build was cut short because the pull request is marked as draft. To complete a full ' +
-                    'build, run the Jenkins job again after marking this pull request as ready for review.'
-        }
-        notifyPrBuildResult(
-                prDetails, repo.organization, repo.name,
-                color,
-                message
-        )
-    } else {
-        echo 'Could not find a corresponding PR to notify from'
-    }
-}
+//oid postBuildPrHook(GitHubRepo gitHubRepo) {
+//    GitHubRepo repo = new GitHubRepo(repoSsh)
+//    BuildManagement buildManagement = TaskLoader.load(binding, BuildManagement)
+//
+//    String prNumber = buildManagement.getPrNumber()
+//    String credentialsId = gitHubRepo.credentialsId
+//
+//    if (prNumber) {
+//        Map<String, Object> prDetails = getPullRequestDetails(
+//                credentialsId,
+//                repo.organization,
+//                repo.name,
+//                prNumber
+//        )
+//        notifyPrReviewer(prDetails)
+//        String color = 'danger'
+//        String message = 'Build Failed.'
+//
+//        if (prDetails.draft) {
+//            color = 'warning'
+//            message = 'This build was cut short because the pull request is marked as draft. To complete a full ' +
+//                    'build, run the Jenkins job again after marking this pull request as ready for review.'
+//        }
+//        notifyPrBuildResult(
+//                prDetails, repo.organization, repo.name,
+//                color,
+//                message
+//        )
+//    } else {
+//        echo 'Could not find a corresponding PR to notify from'
+//    }
+//}
 
