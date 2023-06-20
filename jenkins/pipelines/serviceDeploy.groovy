d21c151843f3419134a7ccd20bce477cda25a642
gdittrick <gdittrick@zevrant-services.com>
2023-06-20 10:12:31 -0400

    added library

--- jenkins/pipelines/serviceDeploy.groovy
+++ jenkins/pipelines/serviceDeploy.groovy
@@ -1,3 +1,5 @@
+@Library("CommonUtils") _
+
 import com.lesfurets.jenkins.unit.global.lib.Library
 import com.zevrant.services.ServiceLoader
 import com.zevrant.services.pojo.KubernetesService
@@ -5,7 +7,7 @@ import com.zevrant.services.pojo.KubernetesServiceCollection
 import com.zevrant.services.services.CertificateService
 import com.zevrant.services.services.GitService
 
-@Library("CommonUtils")
+
 
 KubernetesService service = KubernetesServiceCollection.findServiceByName(SERVICE_NAME as String) as KubernetesService
 GitService gitService = ServiceLoader.load(binding, GitService.class) as GitService
