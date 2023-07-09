35a1e701dd9d2211ae8592d6c4af2c634eada383
gdittrick <gdittrick@zevrant-services.com>
2023-07-09 14:59:53 -0400

    disabling local image repo

--- jenkins/pipelines/kubernetes-deploy.groovy
+++ jenkins/pipelines/kubernetes-deploy.groovy
@@ -64,6 +64,7 @@ pipeline {
                             String postgresPassword = new String(Base64.decoder.decode(credentials.data.password), StandardCharsets.UTF_8)
                             String userPostgresPassword = new String(Base64.decoder.decode(userCredentials.data.password), StandardCharsets.UTF_8)
                             String liquibasePostgresPassword = new String(Base64.decoder.decode(userCredentials.data.liquibasePassword), StandardCharsets.UTF_8)
+                            sh "kubectl rollout status deploy ${codeUnit.name}-postgres-postgresql-ha-pgpool --timeout 5m"
                             container("psql") {
                                 gitService.checkout('zevrant-services-pipeline')
                                 dir('sql') {
