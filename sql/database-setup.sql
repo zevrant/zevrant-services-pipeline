22c45c8dd5c7f7142efae1a66e96d7e5938ca088
gdittrick <gdittrick@zevrant-services.com>
2023-07-10 09:58:51 -0400

    debugging

--- sql/database-setup.sql
+++ sql/database-setup.sql
@@ -3,7 +3,7 @@ create database $DATABASE_NAME;
 \connect $DATABASE_NAME;
 create role app_user NOCREATEROLE NOCREATEDB NOSUPERUSER;
 create role change_management;
-REVOKE ALL ON SCHEMA public FROM user_role;
+REVOKE ALL ON SCHEMA public FROM app_user;
 REVOKE ALL ON SCHEMA public FROM change_management;
 grant SELECT, UPDATE, INSERT to app_user;
 grant update, insert to change_management;
