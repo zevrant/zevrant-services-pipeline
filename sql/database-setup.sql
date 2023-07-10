c558a4c54f8d880e7210658264b66a7151a86ac3
gdittrick <gdittrick@zevrant-services.com>
2023-07-10 09:28:16 -0400

    added database user

--- sql/database-setup.sql
+++ sql/database-setup.sql
@@ -2,13 +2,13 @@ REVOKE ALL ON SCHEMA public FROM public;
 create database $DATABASE_NAME;
 \connect $DATABASE_NAME;
 create role app_user NOCREATEROLE NOCREATEDB NOSUPERUSER;
-create role liquibase;
+create role change_management;
 REVOKE ALL ON SCHEMA public FROM user_role;
-REVOKE ALL ON SCHEMA public FROM liquibase;
+REVOKE ALL ON SCHEMA public FROM change_management;
 grant SELECT, UPDATE, INSERT to app_user;
-grant update, insert to liquibase;
-grant create table to liquibase
+grant update, insert to change_management;
+grant create to change_management
 create user $APP_USER with password $USER_PASSWORD;
 create user liquibase with password $LIQUIBASE_PASSWORD;
 grant app_user to $APP_USER;
-grant liquibase to liquibase;
+grant change_management to liquibase;
