1d2e11c982b2cba9dd81bc45ff64c161a8c39f56
gdittrick <gdittrick@zevrant-services.com>
2023-10-21 08:39:54 -0400

    debugging

--- jenkins/seed.groovy
+++ jenkins/seed.groovy
@@ -66,7 +66,7 @@ SpringCodeUnitCollection.microservices.each { springCodeUnit ->
                     'ENVIRONMENT': 'prod'
             ]),
     )
-//    jobDslService.createPipeline(folder,containerBuild)
+    jobDslService.createPipeline(folder,containerBuild)
 //    jobDslService.createPipeline(folder, developDeployPipeline);
 //    jobDslService.createPipeline(folder, prodDeployPipeline);
 }
