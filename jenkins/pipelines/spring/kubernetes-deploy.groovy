e133e94277dc23557ccc33b663ac4c6394ba5c7f
gdittrick <gdittrick@zevrant-services.com>
2023-11-08 10:23:16 -0500

    added helm deploy

--- jenkins/pipelines/spring/kubernetes-deploy.groovy
+++ jenkins/pipelines/spring/kubernetes-deploy.groovy
@@ -43,116 +43,18 @@ pipeline {
                 }
             }
         }
-        stage("Deploy Database") {
-            when { expression { codeUnit.getPostgresDatabase() } }
-            steps {
-                container('kubectl') {
-                    script {
-                        kubernetesService.requestCertificate("${codeUnit.name}-postgres", ENVIRONMENT, ["${codeUnit.name.toLowerCase()}.${ENVIRONMENT.toLowerCase()}.zevrant-services.internal"])
-                        String ipAddress = kubernetesService.getServiceIp()
-                        String yaml = postgresYamlConfigurer.configurePostgresHelmChart(codeUnit.name, ipAddress)
-                        println yaml
-                        String valuesFileName = 'postgres-values.yml'
-                        writeFile(file: valuesFileName, text: yaml)
-                        int status = sh returnStatus: true, script: "helm list -n $ENVIRONMENT | grep ${codeUnit.name}-postgres > /dev/null"
-                        if (status == 1) {
-                            sh "helm install ${codeUnit.name}-postgres oci://registry-1.docker.io/bitnamicharts/postgresql-ha -f ${valuesFileName} -n ${ENVIRONMENT}"
-                            sh "kubectl get secret -n $ENVIRONMENT -o yaml ${codeUnit.name}-postgres-postgresql-ha-postgresql > credentials.yml"
-                            sh "kubectl get secret -n $ENVIRONMENT -o yaml ${codeUnit.name}-postgres-credentials > user-credentials.yml"
-                            sh "kubectl get secret -n $ENVIRONMENT -o yaml ${codeUnit.name}-postgres-admin-user > new-admin-credentials.yml"
-                            sh "kubectl get secret -n $ENVIRONMENT -o yaml ${codeUnit.name}-postgres-postgresql-ha-pgpool > admin-credentials.yml"
-                            def credentials = readYaml(file: 'credentials.yml')
-                            def userCredentials = readYaml(file: 'user-credentials.yml')
-                            def newAdminCreds = readYaml(file: 'new-admin-credentials.yml')
-                            def adminCreds = readYaml(file: 'admin-credentials.yml')
-
-                            String postgresPassword = new String(Base64.decoder.decode(credentials.data.password), StandardCharsets.UTF_8)
-                            String userPostgresPassword = new String(Base64.decoder.decode(userCredentials.data.password), StandardCharsets.UTF_8)
-                            String liquibasePostgresPassword = new String(Base64.decoder.decode(userCredentials.data.liquibasePassword), StandardCharsets.UTF_8)
-                            String adminPassword = new String(Base64.decoder.decode(newAdminCreds.data.get('admin-password')), StandardCharsets.UTF_8)
-                            String repmgrPassword = new String(Base64.decoder.decode(userCredentials.data.get('repmgr-password')), StandardCharsets.UTF_8)
-                            sh "kubectl rollout status statefulset ${codeUnit.name}-postgres-postgresql-ha-postgresql --timeout 5m -n $ENVIRONMENT"
-                            sh "kubectl rollout status deploy ${codeUnit.name}-postgres-postgresql-ha-pgpool --timeout 5m -n $ENVIRONMENT"
-                            sh "kubectl rollout restart deploy ${codeUnit.name}-postgres-postgresql-ha-pgpool -n $ENVIRONMENT"
-                            sh "kubectl rollout status deploy ${codeUnit.name}-postgres-postgresql-ha-pgpool --timeout 5m -n $ENVIRONMENT"
-                            container("psql") {
-                                gitService.checkout('zevrant-services-pipeline')
-                                dir('sql') {
-
-                                    String databaseSetupScript = readFile('database-setup.sql')
-                                    databaseSetupScript = databaseSetupScript
-                                            .replace('$DATABASE_NAME', codeUnit.getDatabaseName())
-                                            .replace('$APP_NAME', codeUnit.name.replace('-', '_'))
-                                            .replace('$APP_USER', codeUnit.getDatabaseUser())
-                                            .replace('$USER_PASSWORD', userPostgresPassword)
-                                            .replace('$LIQUIBASE_PASSWORD', liquibasePostgresPassword)
-                                            .replace('$ADMIN_PASSWORD', adminPassword)
-                                            .replace('$REPMGR_PASSWORD', repmgrPassword)
-
-                                    println databaseSetupScript
-                                    writeFile(file: "${codeUnit.name}-setup.sql", text: databaseSetupScript)
-                                    withEnv(['PGPASSWORD=' + postgresPassword]) {
-                                        sh "psql -U postgres -h ${codeUnit.name}-postgres-postgresql-ha-pgpool.${ENVIRONMENT} -f ${codeUnit.name}-setup.sql"
-                                    }
-                                }
-                            }
-                            adminCreds.data.put('admin-password', newAdminCreds.data.get('admin-password'))
-                            credentials.data.put('repmgr-password', userCredentials.data.get('repmgr-password'))
-                            credentials.data.put('password', userCredentials.data.get('password'))
-                            sh 'rm -f credentials.yml admin-credentials.yml'
-                            writeYaml(file: 'credentials.yml', data: credentials)
-                            writeYaml(file: 'admin-credentials.yml', data: adminCreds)
-                            sh "kubectl apply -n $ENVIRONMENT -f credentials.yml"
-                            sh "kubectl apply -n $ENVIRONMENT -f admin-credentials.yml"
-                            sh "helm upgrade ${codeUnit.name}-postgres oci://registry-1.docker.io/bitnamicharts/postgresql-ha -n ${ENVIRONMENT} -f ${valuesFileName}"
-                            sh "kubectl rollout restart statefulset ${codeUnit.name}-postgres-postgresql-ha-postgresql -n $ENVIRONMENT"
-                            sh "kubectl rollout status statefulset ${codeUnit.name}-postgres-postgresql-ha-postgresql --timeout 5m -n $ENVIRONMENT"
-                            sh "kubectl rollout restart deploy ${codeUnit.name}-postgres-postgresql-ha-pgpool -n $ENVIRONMENT"
-                            sh "kubectl rollout status deploy ${codeUnit.name}-postgres-postgresql-ha-pgpool --timeout 5m -n $ENVIRONMENT"
-                        } else {
-                            sh "help upgrade ${codeUnit.name}-postgres postgresql -n ${ENVIRONMENT}"
-                        }
-                        sh "kubectl rollout status deployments ${codeUnit.name}-postgres-postgresql-ha-pgpool -n $ENVIRONMENT --timeout=5m"
-                    }
-                }
-            }
-        }
-
-        stage("Configure Service") {
-            when { expression { fileExists("service-${ENVIRONMENT}.yml") } }
-            steps {
-                script {
-                    container('kubectl') {
-                        sh "kubectl apply -f service-${ENVIRONMENT}.yml -n ${ENVIRONMENT}"
-                    }
-                }
-            }
-        }
 
         stage("Deploy Micro Service") {
-            when { expression { fileExists('deployment.yml') && VERSION != null && VERSION != '' } }
+            when { expression {  VERSION != null && VERSION != '' } }
             steps {
                 container('kubectl') {
                     script {
-                        sh "sed -i 's/\$ENVIRONMENT/$ENVIRONMENT/g' ./deployment.yml"
-                        sh "sed -i 's/\$VERSION/$VERSION/g' ./deployment.yml"
-                        sh "sed -i 's/\$REPLICAS/${ENVIRONMENT == 'prod' ? 2 : 1}/g' ./deployment.yml"
-                        String deploymentText = ((String) readFile(file: 'deployment.yml'))
-                        println(deploymentText)
-                        int timeout = kubernetesService.getDeployTimeout(ENVIRONMENT == 'prod' ? 2 : 1)
-                        sh "kubectl apply -n $ENVIRONMENT -f ./deployment.yml"
-                        String deploymentName = ""
+                        sh 'echo $DOCKER_CREDENTIALS_PSW | helm registry login harbor.zevrant-services.internal --username $DOCKER_CREDENTIALS_USR --password-stdin'
                         try {
-                            sh "kubectl get deployment $REPOSITORY -n $ENVIRONMENT"
-                        } catch (Exception ex) {
-                            throw new RuntimeException("Failed to retrieve deployment for repository $REPOSITORY in $ENVIRONMENT")
+                            sh "helm upgrade --install ${codeUnit.name} oci://harbor.zevrant-services.internal/${codeUnit.name} --version ${VERSION} -f ${ENVIRONMENT}-values.yml --wait"
+                        } catch (Exception ignored) {
+                            sh 'helm rollback'
                         }
-//                        try {
-                        sh "kubectl rollout status deployments $REPOSITORY -n $ENVIRONMENT --timeout=${timeout}s"
-//                        } catch (Exception ignored) {
-//                            sh "kubectl rollout undo deploy $REPOSITORY -n $ENVIRONMENT"
-//                            throw new RuntimeException("Deployment for $REPOSITORY in Environment $ENVIRONMENT failed and was rolled back")
-//                        }
                     }
                 }
             }
