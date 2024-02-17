42c307a9cf557f1628aad1176316eb2c72b4f125
gdittrick <gdittrick@zevrant-services.com>
2024-02-17 13:18:23 -0500

    added build cache config

--- jenkins/pipelines/spring/artifact-build.groovy
+++ jenkins/pipelines/spring/artifact-build.groovy
@@ -38,6 +38,14 @@ pipeline {
             steps {
                 script {
                     container('spring-jenkins-slave') {
+                        sh 'echo "buildCache {" >> settings.gradle'
+                        sh 'echo "remote(HttpBuildCache) {" >> settings.gradle'
+                        sh 'echo "url = \'https://build-cache-node:5071/cache/\'" >> settings.gradle'
+                        sh 'echo "allowUntrustedServer = true" >> settings.gradle'
+                        sh 'echo "credentials {" >> settings.gradle'
+                        sh 'echo "username = \'$GRADLE_CACHE_CREDENTIALS_USR\'" >> settings.gradle'
+                        sh 'echo "password = \'$GRADLE_CACHE_CREDENTIALS_PSW\'" >> settings.gradle'
+                        sh 'echo "}}}" >> settings.gradle'
                         sh "CI=ci bash gradlew assemble -x test -x integrationTest --no-watch-fs --build-cache"
                     }
                 }
