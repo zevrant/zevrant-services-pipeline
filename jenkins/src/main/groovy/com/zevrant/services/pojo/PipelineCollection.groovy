062c7c5af82aa0813ef5577e1b08a877259e2a22
gdittrick <gdittrick@zevrant-services.com>
2024-11-27 17:45:41 -0500

    debugging seed job

--- jenkins/src/main/groovy/com/zevrant/services/pojo/PipelineCollection.groovy
+++ jenkins/src/main/groovy/com/zevrant/services/pojo/PipelineCollection.groovy
@@ -96,20 +96,20 @@ class PipelineCollection {
                             )
                     ])
             ),
-            new Pipeline(
-                    name: "packer-seed-job",
-                    folder: 'packer',
-                    description: "Pipeline to build base docker images",
-                    jenkinsfileLocation: "jenkins/pipelines/packer/packer-seed-job.groovy",
-                    credentialId: "jenkins-git",
-                    triggers: new ArrayList<>([
-                            new PipelineTrigger(
-                                    type: PipelineTriggerType.CRON,
-                                    value: "0 3 * * 6")
-                    ]),
-                    envs: [
-                            FOLDER_PATH: 'containers'
-                    ]
-            ),
+//            new Pipeline(
+//                    name: "packer-seed-job",
+//                    folder: 'packer',
+//                    description: "Pipeline to build base docker images",
+//                    jenkinsfileLocation: "jenkins/pipelines/packer/packer-seed-job.groovy",
+//                    credentialId: "jenkins-git",
+//                    triggers: new ArrayList<>([
+//                            new PipelineTrigger(
+//                                    type: PipelineTriggerType.CRON,
+//                                    value: "0 3 * * 6")
+//                    ]),
+//                    envs: [
+//                            FOLDER_PATH: 'containers'
+//                    ]
+//            ),
     ])
 }
