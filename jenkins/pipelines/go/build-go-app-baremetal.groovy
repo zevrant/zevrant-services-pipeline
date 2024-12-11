3ed21083ec0df3de41d96b2362759f7824b69b0f
gdittrick <gdittrick@zevrant-services.com>
2024-12-11 12:50:21 -0500

    testing gpg signing

--- jenkins/pipelines/go/build-go-app-baremetal.groovy
+++ jenkins/pipelines/go/build-go-app-baremetal.groovy
@@ -136,9 +136,14 @@ github.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okW
             environment {
                 GPG_FINGERPRINT = "65B4 607F 0CB9 810D 48F2  012B 0CFB 0807 6C37 70BF"
                 GPG_PASSWORD = credentials('terraform-gpg-password')
+                GPG_KEY = credentials('gpg-key')
             }
             steps {
                 script {
+                    sh 'gpg-agent --batch --daemon'
+                    sh 'gpg -v --batch --import $GPG_KEY'
+                    sh "gpg -v --clearsign main.go"
+
                     //no spillage here as the key var is just the path to the key
                     println 'cleaning up untracked files'
 
