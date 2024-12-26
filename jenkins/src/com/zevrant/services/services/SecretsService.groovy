178f78d98d6c46ae8d4ac2f511df0b5dde1a4330
gdittrick <gdittrick@zevrant-services.com>
2024-12-26 10:08:47 -0500

    updated status definition for http request in secrets service

--- jenkins/src/com/zevrant/services/services/SecretsService.groovy
+++ jenkins/src/com/zevrant/services/services/SecretsService.groovy
@@ -23,7 +23,7 @@ class SecretsService extends Service {
                         ]
                 ],
                 requestBody: 'client_id=' + encodedClientId + '&client_secret=' + encodedClientSecret + '&grant_type=client_credentials&audience=https://api.hashicorp.cloud'
-                status: 200, 201
+                status: '200,201'
         )
 
         return pipelineContext.readJSON(test: response.content).access_token
@@ -49,7 +49,7 @@ class SecretsService extends Service {
                                 value: 'Bearer ' + hcpToken
                         ]
                 ],
-                status: 200
+                status: '200'
         )
 
         pipelineContext.println(response.content)
