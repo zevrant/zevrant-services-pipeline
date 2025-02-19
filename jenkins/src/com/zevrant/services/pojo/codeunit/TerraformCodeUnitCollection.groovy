8c5ae1e835ae67580da0f211bef688d080f941c2
gdittrick <gdittrick@zevrant-services.com>
2025-02-19 10:53:24 -0500

    debugging

--- jenkins/src/com/zevrant/services/pojo/codeunit/TerraformCodeUnitCollection.groovy
+++ jenkins/src/com/zevrant/services/pojo/codeunit/TerraformCodeUnitCollection.groovy
@@ -5,23 +5,33 @@ import com.zevrant.services.enumerations.SecretType
 import com.zevrant.services.pojo.GitRepo
 import com.zevrant.services.pojo.SecretMapping
 
-class TerraformCodeUnitCollection extends CodeUnitCollection<TerraformCodeUnit> {
+class TerraformCodeUnitCollection {
 
-    static {
-        codeUnits = [
-                new TerraformCodeUnit([
-                        name        : 'shared',
-                        repo        : new GitRepo(
-                                'github.com', 'git@github.com',
-                                'zevrant', 'zevrant-services-terraform', 'jenkins-git'
-                        ),
-                        testsEnabled: false,
-                        envs        : [
-                                "shared": [
-                                        new SecretMapping(SecretType.USERNAME_PASSWORD, '/proxmox/jenkins')
-                                ]
-                        ]
-                ])
-        ]
+    private static List<TerraformCodeUnit> codeUnits = [
+            new TerraformCodeUnit([
+                    name        : 'shared',
+                    repo        : new GitRepo(
+                            'github.com', 'git@github.com',
+                            'zevrant', 'zevrant-services-terraform', 'jenkins-git'
+                    ),
+                    testsEnabled: false,
+                    envs        : [
+                            "shared": [
+                                    new SecretMapping(SecretType.USERNAME_PASSWORD, '/proxmox/jenkins')
+                            ]
+                    ]
+            ])
+    ]
+
+    static TerraformCodeUnit findByRepoName(String repoName) {
+        return codeUnits.find({ codeUnit -> codeUnit.getRepo().getRepoName() == repoName })
+    }
+
+    static TerraformCodeUnit findServiceByServiceName(String serviceName) {
+        return codeUnits.find({ codeUnit -> codeUnit.name == serviceName })
+    }
+
+    static List<TerraformCodeUnit> getCodeUnits() {
+        return codeUnits
     }
 }
