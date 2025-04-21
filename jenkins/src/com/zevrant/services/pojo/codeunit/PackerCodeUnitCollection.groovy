07a3622f7f40469061d21a19415005616929fc8e
gdittrick <gdittrick@zevrant-services.com>
2025-04-21 09:43:13 -0400

    bump vm agent version

--- jenkins/src/com/zevrant/services/pojo/codeunit/PackerCodeUnitCollection.groovy
+++ jenkins/src/com/zevrant/services/pojo/codeunit/PackerCodeUnitCollection.groovy
@@ -9,7 +9,7 @@ class PackerCodeUnitCollection {
                     name: 'alma-base-image',
                     extraArguments: [
                             'nodeExporterVersion': '1.8.2',
-                            'vmAgentVersion': '0.0.5'
+                            'vmAgentVersion': '0.0.6'
                     ],
                     folderPath: 'base-vm',
                     repo: new GitRepo(
