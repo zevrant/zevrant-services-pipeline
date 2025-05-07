12549cfe03e925dd091488a667378f2894f0e97d
gdittrick <gdittrick@zevrant-services.com>
2025-05-07 17:12:45 -0400

    added support for nested collections in tfvars config

--- jenkins/src/com/zevrant/services/services/TerraformService.groovy
+++ jenkins/src/com/zevrant/services/services/TerraformService.groovy
@@ -90,12 +90,10 @@ class TerraformService extends Service {
                         throw new RuntimeException("Secret type for vault has not been implemented!")
                 }
 
-            } else if (value instanceof Collection) {
+            } else if (value instanceof Map) {
                 configMappings.add(
                         "TF_VAR_${key}="
-                                + pipelineContext.writeJson(json: value, returnText: true)
-                                .replace(": ", "= ")
-                )
+                                + pipelineContext.writeJson(json: value, returnText: true))
             } else {
                 configMappings.add("TF_VAR_${key}=" + value.toString())
             }
