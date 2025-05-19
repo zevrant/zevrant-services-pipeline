65fbb6ea2fb05b1f4edc155af5a616c7cfae7dbb
gdittrick <gdittrick@zevrant-services.com>
2025-05-19 13:58:33 -0400

    ZBS-41: added additional logging for tfvar population to show which vars are populated

--- jenkins/src/com/zevrant/services/services/TerraformService.groovy
+++ jenkins/src/com/zevrant/services/services/TerraformService.groovy
@@ -96,6 +96,11 @@ class TerraformService extends Service {
                         "TF_VAR_${key}="
                                 + pipelineContext.writeJSON(json: value, returnText: true)
                                 .replace(": ", "= "))
+                if (key == 'dns_zone_configs') {
+                    pipelineContext.println("TF_VAR_${key}="
+                            + pipelineContext.writeJSON(json: value, returnText: true)
+                            .replace(": ", "= "))
+                }
             } else {
                 configMappings.add("TF_VAR_${key}=" + value.toString())
             }
