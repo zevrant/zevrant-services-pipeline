308df78366f8d7d5681917f55875ad19149dc4ba
gdittrick <gdittrick@zevrant-services.com>
2025-05-22 13:53:46 -0400

    switching to repo bound terraform tfvars files to hold non-sensitive values

--- jenkins/src/com/zevrant/services/pojo/codeunit/TerraformCodeUnitCollection.groovy
+++ jenkins/src/com/zevrant/services/pojo/codeunit/TerraformCodeUnitCollection.groovy
@@ -16,144 +16,90 @@ class TerraformCodeUnitCollection {
                     ),
                     testsEnabled: false,
                     envs        : [
-                            "shared"       : [proxmox    : new SecretMapping(SecretType.USERNAME_PASSWORD, '/proxmox/jenkins-token'),
-                                              VAULT_ADDR : 'https://vault.zevrant-services.com',
-                                              VAULT_TOKEN: new SecretMapping(SecretType.VAULT_TOKEN, ''),
+                            "shared"       : [
+                                    proxmox    : new SecretMapping(SecretType.USERNAME_PASSWORD, '/proxmox/jenkins-token'),
+                                    VAULT_ADDR : 'https://vault.zevrant-services.com',
+                                    VAULT_TOKEN: new SecretMapping(SecretType.VAULT_TOKEN, ''),
                                     hcp_client      : new SecretMapping(SecretType.HCP_CLIENT, ''),
                                     CF_DNS_API_TOKEN: new SecretMapping(SecretType.SECRET_TEXT, 'shared/cloudflare-dns-api-token', true),
-                                              trigger    : [
+                                    trigger    : [
                                             type : PipelineTriggerType.CRON,
                                             value: "0 0 * * *"
                                     ]
                             ],
-                            "shared-common": [hcp_client          : new SecretMapping(SecretType.HCP_CLIENT, ''),
-                                              proxmox             : new SecretMapping(SecretType.USERNAME_PASSWORD, '/proxmox/jenkins-token'),
-                                              VAULT_ADDR          : 'https://vault.zevrant-services.com',
-                                              VAULT_TOKEN         : new SecretMapping(SecretType.VAULT_TOKEN, ''),
-                                              "ingress_virtual_ip": "10.1.0.254/24",
-                                              "pki_role"          : [ //TODO use terraform remote state instead of hardcoding
-                                                                      "backend"   : "pki_shared",
-                                                                      "issuer_ref": "7149394f-5f66-0579-fced-27db21503f89",
-                                                                      "name"      : "zevrant-services-shared"
-                                    ],
-                                              MINIO_USER          : new SecretMapping(SecretType.SECRET_TEXT, '/jenkins/minio-username', true),
-                                              MINIO_PASSWORD      : new SecretMapping(SecretType.SECRET_TEXT, '/jenkins/minio-password', true),
-                                              trigger             : [
+                            "shared-common": [
+                                    hcp_client    : new SecretMapping(SecretType.HCP_CLIENT, ''),
+                                    proxmox       : new SecretMapping(SecretType.USERNAME_PASSWORD, '/proxmox/jenkins-token'),
+                                    VAULT_ADDR    : 'https://vault.zevrant-services.com',
+                                    VAULT_TOKEN   : new SecretMapping(SecretType.VAULT_TOKEN, ''),
+                                    MINIO_USER    : new SecretMapping(SecretType.SECRET_TEXT, '/jenkins/minio-username', true),
+                                    MINIO_PASSWORD: new SecretMapping(SecretType.SECRET_TEXT, '/jenkins/minio-password', true),
+                                    trigger       : [
                                             type : PipelineTriggerType.CRON,
                                             value: "0 0 * * *"
                                     ],
-                                    dns_allowed_query_cidr: "10.0.0.0/8",
-                                              dns_zone_configs    : [
-                                            [
-                                                    domain_name  : "zevrant-services.internal",
-                                                    name         : "zevrant-services-shared",
-                                                    nameserver_ip: "10.0.0.150",
-                                                    records      : [
-                                                            [
-                                                                    domain    : "jenkins",
-                                                                    ip_address: "10.0.0.150"
-                                                            ],
-                                                            [
-                                                                    domain    : "plex",
-                                                                    ip_address: "10.0.0.150"
-                                                            ],
-                                                            [
-                                                                    domain    : "vault",
-                                                                    ip_address: "10.0.0.150"
-                                                            ],
-                                                            [
-                                                                    domain    : "vault",
-                                                                    ip_address: "10.1.0.254"
-                                                            ],
-                                                            [
-                                                                    domain    : "s3",
-                                                                    ip_address: "10.1.0.254"
-                                                            ],
-                                                            [
-                                                                    domain    : "s3",
-                                                                    ip_address: "10.0.0.150"
-                                                            ],
-                                                            [
-                                                                    domain    : "s3-console",
-                                                                    ip_address: "10.1.0.7"
-                                                            ],
-                                                            [
-                                                                    domain    : "garage1",
-                                                                    ip_address: "10.1.0.7"
-                                                            ],
-                                                            [
-                                                                    domain    : "garage2",
-                                                                    ip_address: "10.1.0.8"
-                                                            ],
-                                                            [
-                                                                    domain    : "garage3",
-                                                                    ip_address: "10.1.0.9"
-                                                            ]
-                                                    ]
-                                            ]
-                                    ]
 
                             ],
-                            "shared-blue"  : [trigger: [
-                                            type : PipelineTriggerType.UPSTREAM,
-                                            value: 'shared-deploy-to-shared-green'
-                                    ],
-                                    proxmox       : new SecretMapping(SecretType.USERNAME_PASSWORD, '/proxmox/jenkins-token'),
-                                    VAULT_ADDR    : 'https://vault.zevrant-services.com',
-                                    VAULT_TOKEN   : new SecretMapping(SecretType.VAULT_TOKEN, ''),
-                                    load_balancers: [
-                                            "01": [
-                                                    cpu              : 2,
-                                                    default_user     : "zevrant",
-                                                    description: "Keepalived haproxy",
-                                                    gateway          : "10.1.0.1",
-                                                    hostname         : "haproxy-shared-blue",
-                                                    ip_address       : "10.1.0.5/24",
-                                                    is_primary       : true,
-                                                    mass_storage_name: "exosDisks",
-                                                    memory_mbs       : 4096,
-                                                    nameserver       : "10.0.0.8",
-                                                    peer_ip_addresses: [
-                                                            "10.1.0.12"
-                                                    ],
-                                                    proxmox_host     : "proxmox-01",
-                                                    replica_priority : 255,
-                                                    ssd_storage_name : "local-zfs",
-                                                    vm_id            : 1020
-                                            ]
-                                    ],
-                                    MINIO_USER    : new SecretMapping(SecretType.SECRET_TEXT, '/jenkins/minio-username', true),
-                                    MINIO_PASSWORD: new SecretMapping(SecretType.SECRET_TEXT, '/jenkins/minio-password', true),
-                                    dns_servers   : [
-                                            'dns_servers': [
-                                                    "01": [
-                                                            cpu              : 2,
-                                                            default_user     : "zevrant",
-                                                            description      : "top level dns domain",
-                                                            gateway          : "10.0.0.1",
-                                                            hostname         : "internal-bind9",
-                                                            ip_address: "10.0.0.8/24",
-                                                            mass_storage_name: "exosDisks",
-                                                            memory_mbs       : 4096,
-                                                            nameserver       : "1.1.1.1",
-                                                            protection       : false,
-                                                            proxmox_host     : "proxmox-01",
-                                                            ssd_storage_name : "local-zfs",
-                                                            start_on_boot    : true,
-                                                            vm_id     : 1030,
-                                                    ]
-                                            ]
-                                    ]
+                            "shared-blue"  : [trigger       : [
+                                    type : PipelineTriggerType.UPSTREAM,
+                                    value: 'shared-deploy-to-shared-green'
                             ],
-                            "shared-green" : [trigger: [
-                                            type : PipelineTriggerType.UPSTREAM,
-                                            value: 'shared-deploy-to-shared-common'
-                                    ],
-                                    proxmox       : new SecretMapping(SecretType.USERNAME_PASSWORD, '/proxmox/jenkins-token'),
-                                    VAULT_ADDR    : 'https://vault.zevrant-services.com',
-                                    VAULT_TOKEN   : new SecretMapping(SecretType.VAULT_TOKEN, ''),
-                                    MINIO_USER    : new SecretMapping(SecretType.SECRET_TEXT, '/jenkins/minio-username', true),
-                                    MINIO_PASSWORD: new SecretMapping(SecretType.SECRET_TEXT, '/jenkins/minio-password', true),
+                                              proxmox       : new SecretMapping(SecretType.USERNAME_PASSWORD, '/proxmox/jenkins-token'),
+                                              VAULT_ADDR    : 'https://vault.zevrant-services.com',
+                                              VAULT_TOKEN   : new SecretMapping(SecretType.VAULT_TOKEN, ''),
+                                              load_balancers: [
+                                                      "01": [
+                                                              cpu              : 2,
+                                                              default_user     : "zevrant",
+                                                              description      : "Keepalived haproxy",
+                                                              gateway          : "10.1.0.1",
+                                                              hostname         : "haproxy-shared-blue",
+                                                              ip_address       : "10.1.0.5/24",
+                                                              is_primary       : true,
+                                                              mass_storage_name: "exosDisks",
+                                                              memory_mbs       : 4096,
+                                                              nameserver       : "10.0.0.8",
+                                                              peer_ip_addresses: [
+                                                                      "10.1.0.12"
+                                                              ],
+                                                              proxmox_host     : "proxmox-01",
+                                                              replica_priority : 255,
+                                                              ssd_storage_name : "local-zfs",
+                                                              vm_id            : 1020
+                                                      ]
+                                              ],
+                                              MINIO_USER    : new SecretMapping(SecretType.SECRET_TEXT, '/jenkins/minio-username', true),
+                                              MINIO_PASSWORD: new SecretMapping(SecretType.SECRET_TEXT, '/jenkins/minio-password', true),
+                                              dns_servers   : [
+                                                      'dns_servers': [
+                                                              "01": [
+                                                                      cpu              : 2,
+                                                                      default_user     : "zevrant",
+                                                                      description      : "top level dns domain",
+                                                                      gateway          : "10.0.0.1",
+                                                                      hostname         : "internal-bind9",
+                                                                      ip_address       : "10.0.0.8/24",
+                                                                      mass_storage_name: "exosDisks",
+                                                                      memory_mbs       : 4096,
+                                                                      nameserver       : "1.1.1.1",
+                                                                      protection       : false,
+                                                                      proxmox_host     : "proxmox-01",
+                                                                      ssd_storage_name : "local-zfs",
+                                                                      start_on_boot    : true,
+                                                                      vm_id            : 1030,
+                                                              ]
+                                                      ]
+                                              ]
+                            ],
+                            "shared-green" : [trigger       : [
+                                    type : PipelineTriggerType.UPSTREAM,
+                                    value: 'shared-deploy-to-shared-common'
+                            ],
+                                              proxmox       : new SecretMapping(SecretType.USERNAME_PASSWORD, '/proxmox/jenkins-token'),
+                                              VAULT_ADDR    : 'https://vault.zevrant-services.com',
+                                              VAULT_TOKEN   : new SecretMapping(SecretType.VAULT_TOKEN, ''),
+                                              MINIO_USER    : new SecretMapping(SecretType.SECRET_TEXT, '/jenkins/minio-username', true),
+                                              MINIO_PASSWORD: new SecretMapping(SecretType.SECRET_TEXT, '/jenkins/minio-password', true),
                             ]
                     ]
             ]),
