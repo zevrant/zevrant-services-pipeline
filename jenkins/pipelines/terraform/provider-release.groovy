fbc0e818b56be3415093ca6d645612c1ff8f14dd
gdittrick <gdittrick@zevrant-services.com>
2025-12-18 10:39:03 -0500

    fixed quote balancing for curl request

--- jenkins/pipelines/terraform/provider-release.groovy
+++ jenkins/pipelines/terraform/provider-release.groovy
@@ -42,7 +42,7 @@ pipeline {
                             //Http request plugin doesn't support following redirects
 
                             sh 'curl -s --request GET -L --url https://github.com/zevrant/' + codeUnit.name + '/releases/download/' + taggedVersion + '/' + codeUnit.name + '_' + taggedVersion.replace('v', '') + '_SHA256SUMS   --header \'Authorization: bearer ' + token.replace('"', '') + '\' -o ' + codeUnit.name + '_' + taggedVersion.replace('v', '') + '_SHA256SUMS'
-                            sh 'curl -s --request GET -L --url https://github.com/zevrant/' + codeUnit.name + '/releases/download/' + taggedVersion + '/' + codeUnit.name + '_' + taggedVersion.replace('v', '') '_SHA256SUMS.sig   --header \'Authorization: bearer ' +token.replace('"', '') + ' -o ' + codeUnit.name + '_' + taggedVersion.replace('v', '') + '_SHA256SUMS.sig'
+                            sh 'curl -s --request GET -L --url https://github.com/zevrant/' + codeUnit.name + '/releases/download/' + taggedVersion + '/' + codeUnit.name + '_' + taggedVersion.replace('v', '') + '_SHA256SUMS.sig   --header \'Authorization: bearer ' + token.replace('"', '') + '\' -o ' + codeUnit.name + '_' + taggedVersion.replace('v', '') + '_SHA256SUMS.sig'
 
                         }
                     }
