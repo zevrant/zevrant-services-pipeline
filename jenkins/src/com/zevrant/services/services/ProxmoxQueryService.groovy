13448c62c1b13894855f2e6450181c013bd55035
gdittrick <gdittrick@zevrant-services.com>
2025-12-21 13:15:44 -0500

    added missing image path

--- jenkins/src/com/zevrant/services/services/ProxmoxQueryService.groovy
+++ jenkins/src/com/zevrant/services/services/ProxmoxQueryService.groovy
@@ -97,7 +97,8 @@ public class ProxmoxQueryService extends Service {
                     ' --url \'https://' + proxmoxNode + '.zevrant-services.com:8006/api2/json/nodes/' + proxmoxNode + '/storage/vm-images/upload' + params + '\' '
                     + ' --header \'Authorization: PVEAPIToken=' + username + '=' + password + '\''
                     + ' --header \'Content-Type: multipart/form-data\''
-                    + ' --header \'User-Agent: insomnia/11.6.1\'')
+                    + ' --header \'User-Agent: insomnia/11.6.1\''
+                    + ' --form filename=' + imagePath)
         }
         def taskId = pipelineContext.readJSON(text: taskIdJsonString)
         if (!waitForTaskCompletion(proxmoxNode, taskId.data)) {
